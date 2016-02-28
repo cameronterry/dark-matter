3021b10d9db6a0c3a0ddd9d43720ce4825d574a7
Cameron Terry <cammy.wan.kenobi@gmail.com>
2016-02-28 20:29:48 +0000

    Added the form submission JavaScript logic.

--- ui/js/dark-matter.js
+++ ui/js/dark-matter.js
@@ -1,7 +1,25 @@
 ( function ( $, window, undefined ) {
 
   $( document ).ready( function () {
-    
+
+    $( '#dm_new_add_domain' ).click( function ( e ) {
+      var data = $( '#dm_add_domain_form' ).serializeArray();
+
+      data['action'] = 'dark_matter_add_domain';
+      data['nonce'] = $( '#dm_new_nonce' ).val();
+
+      $.post( ajaxurl, data, function ( response ) {
+        if ( false === data.success ) {
+          alert( data.error_message );
+        }
+        else {
+          var template = wp.template( 'domain-row' );
+        }
+      } );
+
+      e.preventDefault();
+    } );
+
   } );
 
 } )( jQuery, window );
