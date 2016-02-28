e2df5eb29deba5e25f0019b628e28b0ca2d59be6
Cameron Terry <cammy.wan.kenobi@gmail.com>
2016-02-28 18:09:19 +0000

    Got the table for the Domain Mapping looking decent enough to work with.

--- ui/blog.php
+++ ui/blog.php
@@ -9,8 +9,33 @@ function dark_matter_blog_admin_menu() {
 add_action( 'admin_menu', 'dark_matter_blog_admin_menu' );
 
 function dark_matter_blog_domain_mapping() { ?>
-  <div class="wrap">
+  <div class="wrap dark-matter-blog">
     <h1><?php _e( 'Domain Mapping for this Blog', 'darkmatter' ); ?></h1>
-    <table></table>
+    <h2><?php _e( 'Mapped Domains', 'darkmatter' ); ?></h2>
+    <table>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Domain</th>
+          <th>Is Main?</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>
+            <a href="#">siteone.test</a>
+          </td>
+          <td>
+            Yes
+          </td>
+          <td>
+            Delete
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <h2><?php _e( 'Add New Domain', 'darkmatter' ); ?></h2>
   </div>
 <?php }
