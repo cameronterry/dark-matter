916358771ba8615e558f8c9f087f7f3cb88d7e15
Cameron Terry <cammy.wan.kenobi@gmail.com>
2016-03-01 20:41:46 +0000

    Got the basic AJAX handling in the JavaScript sorted out for the Blog domain page.

--- ui/js/dark-matter.js
+++ ui/js/dark-matter.js
@@ -5,15 +5,25 @@
     $( '#dm_new_add_domain' ).click( function ( e ) {
       var data = $( '#dm_add_domain_form' ).serializeArray();
 
-      data['action'] = 'dark_matter_add_domain';
-      data['nonce'] = $( '#dm_new_nonce' ).val();
+      data.push( {
+        'name' : 'action',
+        'value' : 'dark_matter_add_domain'
+      } );
+
+      console.log( data );
 
       $.post( ajaxurl, data, function ( response ) {
-        if ( false === data.success ) {
-          alert( data.error_message );
+        console.log( response );
+
+        if ( false === response.success ) {
+          alert( response.error_message );
         }
         else {
           var template = wp.template( 'domain-row' );
+          var $this = $( template( response.data ) );
+
+          $( '.dark-matter-blog table > tbody' ).append( $this );
+          $this.slideDown( 'fast' );
         }
       } );
 
