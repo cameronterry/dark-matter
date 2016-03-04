775783db838318e49c551e1353ab0076430e18e3
Cameron Terry <cammy.wan.kenobi@gmail.com>
2016-03-04 18:39:28 +0000

    The front end now redirects if it is using the original domain and not the primary mapped domain.

--- inc/redirects.php
+++ inc/redirects.php
@@ -33,3 +33,38 @@ function dark_matter_admin_redirect() {
 }
 add_action( 'admin_init', 'dark_matter_admin_redirect' );
 add_action( 'login_init', 'dark_matter_admin_redirect' );
+
+function dark_matter_frontend_redirect() {
+  /** If it's the main site in the network, do not redirect. */
+  if ( is_main_site() ) {
+    return;
+  }
+
+  /** If we are viewing in the WP Customizer, then we don't want to redirect either. */
+  global $wp_customize;
+
+  if ( is_a( $wp_customize, 'WP_Customize_Manager' ) ) {
+    return;
+  }
+
+  global $current_blog, $wpdb;
+
+  $primary_domain = $wpdb->get_var( $wpdb->prepare( "SELECT domain FROM {$wpdb->dmtable} WHERE blog_id = %s", get_current_blog_id() ) );
+
+  /** No domain has been mapped. */
+  if ( empty( $primary_domain ) ) {
+    return;
+  }
+
+  if ( $primary_domain === $_SERVER['HTTP_HOST'] ) {
+    return;
+  }
+
+  $scheme = ( is_ssl() ? 'https://' : 'http://' );
+  $path = str_replace( $current_blog->path, '', $_SERVER['REQUEST_URI'] );
+  $redirect_url = sprintf( '%1$s%2$s/%3$s', $scheme, $primary_domain, $path );
+
+  wp_redirect( $redirect_url );
+  return;
+}
+add_action( 'template_redirect', 'dark_matter_frontend_redirect' );
