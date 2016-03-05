8421fbdc87fdefd0f3709b392c8a8e9f4e7354fb
Cameron Terry <cammy.wan.kenobi@gmail.com>
2016-03-05 09:21:05 +0000

    Now only attach the actions and filters if the Domain Mapping is actually engaged.

--- inc/urls.php
+++ inc/urls.php
@@ -1,30 +1,32 @@
 <?php
 
 function dark_matter_map_url( $setting ) {
-  if ( defined( 'DOMAIN_MAPPING' ) && DOMAIN_MAPPING ) {
-    global $current_blog;
+  global $current_blog;
 
-    $protocol = ( is_ssl() ? 'https://' : 'http://' );
+  $protocol = ( is_ssl() ? 'https://' : 'http://' );
 
-    return sprintf( '%1$s%2$s', $protocol, $current_blog->domain );
-  }
-
-  return $setting;
+  return sprintf( '%1$s%2$s', $protocol, $current_blog->domain );
 }
-add_filter( 'pre_option_siteurl', 'dark_matter_map_url' );
-add_filter( 'pre_option_home', 'dark_matter_map_url' );
 
 function dark_matter_map_content( $content ) {
-  if ( defined( 'DOMAIN_MAPPING' ) && DOMAIN_MAPPING ) {
-    global $current_blog;
+  global $current_blog;
 
-    $protocol = ( is_ssl() ? 'https://' : 'http://' );
-    $domain = sprintf( '%1$s%2$s/', $protocol, $current_blog->domain );
-    $content = preg_replace( "#http?://{$current_blog->original_domain}#", $domain, $content );
-
-    return $content;
-  }
+  $protocol = ( is_ssl() ? 'https://' : 'http://' );
+  $domain = sprintf( '%1$s%2$s/', $protocol, $current_blog->domain );
+  $content = preg_replace( "#http?://{$current_blog->original_domain}#", $domain, $content );
 
   return $content;
 }
-add_filter( 'the_content', 'dark_matter_map_content' );
+if ( defined( 'DOMAIN_MAPPING' ) && DOMAIN_MAPPING ) {
+  add_filter( 'pre_option_siteurl', 'dark_matter_map_url' );
+  add_filter( 'pre_option_home', 'dark_matter_map_url' );
+  
+  add_filter( 'the_content', 'dark_matter_map_content' );
+  add_filter( 'stylesheet_uri', 'dark_matter_map_content' );
+  add_filter( 'stylesheet_directory', 'dark_matter_map_content' );
+  add_filter( 'stylesheet_directory_uri', 'dark_matter_map_content' );
+  add_filter( 'template_directory', 'dark_matter_map_content' );
+  add_filter( 'template_directory_uri', 'dark_matter_map_content' );
+  add_filter( 'plugins_url', 'dark_matter_map_content' );
+  add_filter( 'wp_get_attachment_url', 'dark_matter_map_content' );
+}
