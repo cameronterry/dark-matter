000abac65763af985e5abf57e3e6e76168a758fb
Cameron Terry <cammy.wan.kenobi@gmail.com>
2016-03-05 10:58:02 +0000

    Wrote a new API which can retrieve the primary domain from the database.

--- inc/api.php
+++ inc/api.php
@@ -34,7 +34,7 @@ function dark_matter_api_del_domain( $id ) {
 function dark_matter_api_get_domain( $identifier = null, $domain_only = false ) {
   global $wpdb;
   $sql = "SELECT * FROM {$wpdb->dmtable} WHERE ";
-  
+
   if ( null === $identifier && empty( $identifier ) ) {
     return false;
   }
@@ -54,6 +54,13 @@ function dark_matter_api_get_domain_original() {
   }
 
   return $current_blog->domain . $current_blog->path;
+}
+
+function dark_matter_api_get_domain_primary( $blog_id ) {
+  $blog_id = ( null === $blog_id ? get_current_blog_id() : $blog_id );
+
+  global $wpdb;
+  return $wpdb->get_row( $wpdb->prepare( "SELECT * FROM {$wpdb->dmtable} WHERE blog_id = %s LIMIT 0, 1", $blog_id ) );
 }
 
 function dark_matter_api_get_domains( $blog_id = null ) {
