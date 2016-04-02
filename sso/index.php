965c13eab799c85de8fcca5d02a57f9e5a2be8e3
Cameron Terry <cammy.wan.kenobi@gmail.com>
2016-04-02 17:45:00 +0100

    Added an endpoint for SSO but it seems the overall plugin has stopped working... great! :-/

--- sso/index.php
+++ sso/index.php
@@ -6,3 +6,23 @@
  * - http://stackoverflow.com/questions/342378/cross-domain-login-how-to-login-a-user-automatically-when-transferred-from-one
  * - https://github.com/markoheijnen/wordpress-mu-domain-mapping/blob/master/domain_mapping.php#L758-L814
  */
+function dark_matter_sso_endpoint() {
+  add_rewrite_endpoint( 'sso', EP_ALL );
+}
+add_action( 'init', 'dark_matter_sso_endpoint' );
+
+function dark_matter_sso_template() {
+  global $wp_query;
+
+  if ( array_key_exists( 'sso', $wp_query->query_vars ) ) {
+    header( 'Content-Type: application/javascript' );
+    header( 'Etag: hello-world' );
+    exit;
+  }
+}
+add_action( 'template_redirect', 'dark_matter_sso_template' );
+
+function dark_matter_sso_wp_head() { ?>
+  <script type="text/javascript" src="http://wordpressnetwork.test/sso/"></script>
+<?php }
+add_action( 'wp_head', 'dark_matter_sso_wp_head' );
