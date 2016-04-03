8fad50dfff0e869aa0282a3b00bd310e369df036
Cameron Terry <cammy.wan.kenobi@gmail.com>
2016-04-03 13:14:15 +0100

    Finally got a basic SSO working with third party cookies and therefore we have an all round first take of a domain mapping plugin.

--- sso/index.php
+++ sso/index.php
@@ -11,18 +11,46 @@ function dark_matter_sso_endpoint() {
 }
 add_action( 'init', 'dark_matter_sso_endpoint' );
 
+function dark_matter_sso_plugins_loaded() {
+	if ( 'authorise' === filter_input( INPUT_GET, '__dm_action' ) ) {
+		$user_id = wp_validate_auth_cookie( filter_input( INPUT_GET, 'auth' ), 'auth' );
+
+		if ( false === $user_id ) {
+			wp_die( 'Oops! Something went wrong with logging in.' );
+		}
+		else {
+			wp_set_auth_cookie( $user_id );
+
+			wp_redirect( esc_url( remove_query_arg( array( '__dm_action', 'auth' ) ) ) );
+			exit;
+		}
+	}
+}
+add_action( 'plugins_loaded', 'dark_matter_sso_plugins_loaded' );
+
 function dark_matter_sso_template() {
   global $wp_query;
 
   if ( array_key_exists( 'sso', $wp_query->query_vars ) ) {
-    header( 'Content-Type: application/javascript' );
-    header( 'Etag: hello-world' );
+    header( 'Content-Type: text/javascript' );
+
+	if ( is_user_logged_in() ) {
+		$url = add_query_arg( array(
+			'__dm_action' => 'authorise',
+			'auth' => wp_generate_auth_cookie( get_current_user_id(), time() + ( 2 * MINUTE_IN_SECONDS ) )
+		), $_SERVER['HTTP_REFERER'] );
+
+		printf( 'window.location.replace("%s");', esc_url_raw( $url ) );
+	}
+
     exit;
   }
 }
 add_action( 'template_redirect', 'dark_matter_sso_template' );
 
-function dark_matter_sso_wp_head() { ?>
-  <script type="text/javascript" src="http://wordpressnetwork.test/sso/"></script>
-<?php }
+function dark_matter_sso_wp_head() {
+	if ( false === is_user_logged_in() && 1 === get_current_blog_id() ) : ?>
+	<script type="text/javascript" src="<?php echo( network_site_url( '/sso/' ) ); ?>"></script>
+<?php endif;
+}
 add_action( 'wp_head', 'dark_matter_sso_wp_head' );
