e926ec804f9aef33f23d8bc42846927d5e6a94d0
Cameron Terry <cammy.wan.kenobi@gmail.com>
2016-06-27 16:35:41 +0100

    Switched the indentation from spaces to tabs on the dark-matter.php file.

--- dark-matter.php
+++ dark-matter.php
@@ -26,46 +26,46 @@ require_once( DM_PATH . '/ui/network.php' );
 require_once( DM_PATH . '/sso/index.php' );
 
 function dark_matter_enqueue_scripts( $hook ) {
-  if ( 'settings_page_dark_matter_blog_settings' === $hook ) {
-    wp_enqueue_script( 'dark-matter-js', plugin_dir_url( __FILE__ ) . 'ui/js/dark-matter.js', array( 'jquery', 'wp-util' ), DM_VERSION );
-    wp_enqueue_style( 'dark-matter-css', plugin_dir_url( __FILE__ ) . 'ui/css/blog.css', null, DM_VERSION );
-  }
+	if ( 'settings_page_dark_matter_blog_settings' === $hook ) {
+		wp_enqueue_script( 'dark-matter-js', plugin_dir_url( __FILE__ ) . 'ui/js/dark-matter.js', array( 'jquery', 'wp-util' ), DM_VERSION );
+		wp_enqueue_style( 'dark-matter-css', plugin_dir_url( __FILE__ ) . 'ui/css/blog.css', null, DM_VERSION );
+	}
 }
 add_action( 'admin_enqueue_scripts', 'dark_matter_enqueue_scripts' );
 
 function dark_matter_maybe_create_tables() {
-  global $wpdb;
+	global $wpdb;
 
-  /** As dbDelta function is called before the upgrade file is included. */
-  require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+	/** As dbDelta function is called before the upgrade file is included. */
+	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
 
-  $charset_collate = $wpdb->get_charset_collate();
+	$charset_collate = $wpdb->get_charset_collate();
 
-  $sql = "CREATE TABLE `{$wpdb->dmtable}` (
-    `id` BIGINT(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `blog_id` BIGINT(20) NOT NULL,
-    `is_primary` TINYINT(4) DEFAULT '0',
-    `domain` VARCHAR(255) NOT NULL,
-    `active` TINYINT(4) DEFAULT '1',
-    `is_https` TINYINT(4) DEFAULT '0'
-  ) $charset_collate;";
+	$sql = "CREATE TABLE `{$wpdb->dmtable}` (
+		`id` BIGINT(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+		`blog_id` BIGINT(20) NOT NULL,
+		`is_primary` TINYINT(4) DEFAULT '0',
+		`domain` VARCHAR(255) NOT NULL,
+		`active` TINYINT(4) DEFAULT '1',
+		`is_https` TINYINT(4) DEFAULT '0'
+	) $charset_collate;";
 
-  dbDelta( $sql );
+	dbDelta( $sql );
 }
 
 function dark_matter_maybe_upgrade() {
-  if ( is_network_admin() ) {
-    $current_version = get_network_option( null, 'dark_matter_version', null );
+	global $dm_current_version;
 
-    if ( DM_VERSION !== $current_version ) {
-      dark_matter_maybe_create_tables();
-      update_network_option( null, 'dark_matter_version', DM_VERSION );
-    }
-  }
+	if ( is_network_admin() ) {
+		if ( DM_VERSION !== $dm_current_version ) {
+			dark_matter_maybe_create_tables();
+			update_network_option( null, 'dark_matter_version', DM_VERSION );
+		}
+	}
 }
 
 function dark_matter_plugins_loaded() {
-  dark_matter_maybe_upgrade();
+	dark_matter_maybe_upgrade();
 }
 add_action( 'plugins_loaded', 'dark_matter_plugins_loaded' );
 
