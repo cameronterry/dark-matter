2d7c7e87cb35ef50d70b5744732a503f187860a7
Cameron Terry <cammy.wan.kenobi@gmail.com>
2016-07-18 08:24:32 +0100

    Make sure the mapped domain actually returns a database record rather than attempt to access properties of "null".

--- dark-matter.php
+++ dark-matter.php
@@ -82,8 +82,10 @@ function dark_matter_prepare() {
 	if ( null !== $dm_current_version ) {
 		$mapped_domain = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM {$wpdb->dmtable} WHERE blog_id = %s AND is_primary = 1 LIMIT 0, 1", $current_blog->blog_id ) );
 
-		$current_blog->https = boolval( $mapped_domain->is_https );
-		$current_blog->primary_domain = $mapped_domain->domain;
+		if ( false === empty( $mapped_domain ) ) {
+			$current_blog->https = boolval( $mapped_domain->is_https );
+			$current_blog->primary_domain = $mapped_domain->domain;
+		}
 	}
 
 	/** Check to see if the Original Domain is present and if not, set it. */
