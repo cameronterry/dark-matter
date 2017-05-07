fc76af189db6c7fe0fabdc297320ff73fef4946c
Cameron Terry <cammy.wan.kenobi@gmail.com>
2017-05-07 16:02:49 +0100

    Updated all the version numbers to be 1.0.0 Release Candidate 8.

--- dark-matter.php
+++ dark-matter.php
@@ -3,7 +3,7 @@
  * Plugin Name: Dark Matter
  * Plugin URI: https://cameronterry.supernovawp.com/dark-matter/
  * Description: A domain mapping plugin from Project Dark Matter.
- * Version: 1.0.0 RC7
+ * Version: 1.0.0 RC8
  * Author: Cameron Terry
  * Author URI: https://cameronterry.supernovawp.com/
  * Text Domain: dark-matter
@@ -28,7 +28,7 @@ defined( 'ABSPATH' ) or die();
 
 /** Setup the Plugin Constants */
 define( 'DM_PATH', plugin_dir_path( __FILE__ ) );
-define( 'DM_VERSION', '1.0.0 RC7' );
+define( 'DM_VERSION', '1.0.0-rc8' );
 define( 'DM_DB_VERSION', '20170109' );
 
 /**
