18242c7a3d8dbfaba13c9c2118bc56391cf81350
Cameron Terry <cammy.wan.kenobi@gmail.com>
2018-03-03 10:40:05 +0000

    Put in a fix to prevent WP-CLI being redirected by Dark Matter.

--- inc/redirects.php
+++ inc/redirects.php
@@ -80,6 +80,14 @@ function dark_matter_redirect_url( $domain, $is_https ) {
 }
 
 function dark_matter_main_redirect() {
+	/**
+	 * Make sure that Dark Matter does not attempt redirects whilst WordPress is
+	 * running inside the WP-CLI.
+	 */
+	if ( defined( 'WP_CLI' ) && WP_CLI ) {
+		return;
+	}
+	
 	/** If Preview, then exit and let the wp action hook handle it. */
 	if ( array_key_exists( 'preview', $_GET ) || array_key_exists( 'p', $_GET ) ) {
 		return;
