8ab51ef4302d5f8607e69263336e263993e98fae
Cameron Terry <cammy.wan.kenobi@gmail.com>
2018-12-30 22:14:02 +0000

    Implemented the CLI add and remove commands for Reserved Domains.

--- cli/reserve.php
+++ cli/reserve.php
@@ -17,7 +17,20 @@ class DarkMatter_Reserve_CLI {
      *      wp darkmatter reserve add www.example.com
      */
     public function add( $args, $assoc_args ) {
+        if ( empty( $args[0] ) ) {
+            WP_CLI::error( __( 'Please include a fully qualified domain name to be added.', 'dark-matter' ) );
+        }
 
+        $fqdn = $args[0];
+
+        $reserved = DarkMatter_Reserve::instance();
+        $result   = $reserved->add( $fqdn );
+
+        if ( is_wp_error( $result ) ) {
+            WP_CLI::error( $result->get_error_message() );
+        }
+
+        WP_CLI::success( $fqdn . __( ': is now reserved.', 'dark-matter' ) );
     }
 
     /**
@@ -34,7 +47,20 @@ class DarkMatter_Reserve_CLI {
      *      wp darkmatter reserve remove www.example.com
      */
     public function remove( $args, $assoc_args ) {
+        if ( empty( $args[0] ) ) {
+            WP_CLI::error( __( 'Please include a fully qualified domain name to be added.', 'dark-matter' ) );
+        }
 
+        $fqdn = $args[0];
+
+        $reserved = DarkMatter_Reserve::instance();
+        $result   = $reserved->delete( $fqdn );
+
+        if ( is_wp_error( $result ) ) {
+            WP_CLI::error( $result->get_error_message() );
+        }
+
+        WP_CLI::success( $fqdn . __( ': is no longer reserved.', 'dark-matter' ) );
     }
 }
 WP_CLI::add_command( 'darkmatter reserve', 'DarkMatter_Reserve_CLI' );
