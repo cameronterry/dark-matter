95a7c252e3c91e0dcc4225211c35cecd8a69e1cb
Cameron Terry <cammy.wan.kenobi@gmail.com>
2019-01-01 06:10:56 +0000

    Added more conditions for when the redirect shouldn't kick in.

--- inc/redirect.php
+++ inc/redirect.php
@@ -2,6 +2,22 @@
 
 defined( 'ABSPATH' ) or die();
 
+/**
+ * The function `rest_get_url_prefix()` is not available at this point in the
+ * load process. Therefore we must substitute it with a close approximate of
+ * what the function does.
+ *
+ * There is a side-effect of this. Basically if a site is to be setup with a
+ * different prefix, in order for this to work, the `add_filter()` call would
+ * need to be done in an Must-Use Plugin.
+ */
+$rest_url_prefix = '/' . trim( apply_filters( 'rest_url_prefix', 'wp-json' ), '/' ) . '/';
+
+/**
+ * Stop the execution of PHP code if any of the following conditions are true.
+ * The constants noted below are all available and prior to the loading of the
+ * sunrise.php file.
+ */
 if (
         /**
          * Do not attempt to redirect during the CLI command.
@@ -9,15 +25,35 @@ if (
         defined( 'WP_CLI' ) && WP_CLI
     ||
         /**
+         * AJAX requests can be used on both the mapped and unmapped domains.
+         */
+        defined( 'DOING_AJAX' ) && DOING_AJAX
+    ||
+        /**
          * Do not attempt to redirect during the execution of cron.
          */
         defined( 'DOING_CRON' ) && DOING_CRON
+    ||
+        /**
+         * XMLRPC Requests can be used on both the mapped and unmapped domains.
+         */
+        defined( 'XMLRPC_REQUEST' ) && XMLRPC_REQUEST
+    ||
+        /**
+         * REST API can be used on both the mapped and unmapped domains.
+         */
+        false !== strpos( $_SERVER['REQUEST_URI'], $rest_url_prefix )
     ||
         /**
          * Do not redirect Admin area. For note; this returns false if the
          * unmapped URL is called.
          */
         is_admin()
+    ||
+        /**
+         * Customizer is presented in an <iframe> over the unmapped domain.
+         */
+        ! empty( $_GET['customize_changeset_uuid'] )
 ) {
     return;
 }
@@ -29,7 +65,19 @@ if (
  * @return void
  */
 function darkmatter_maybe_redirect() {
-    global $dm_original_path;
+    $request = ltrim( $_SERVER['REQUEST_URI'], '/' );
+
+    /**
+     * Determine if the request is one we shouldn't handle for redirects.
+     */
+    $filename = basename( $request );
+    $filename = strtok( $filename, '?' );
+
+    if ( in_array( $filename, array( 'wp-login.php', 'wp-register.php' ) ) ) {
+        return;
+    }
+
+    var_dump( $GLOBALS );die;
 
     /**
      * Retrieve the original blog details. We use this technique rather than
@@ -40,7 +88,6 @@ function darkmatter_maybe_redirect() {
     $original_blog = get_site( get_current_blog_id() );
 
     $host    = trim( $_SERVER['HTTP_HOST'], '/' );
-    $request = ltrim( $_SERVER['REQUEST_URI'], '/' );
     $primary = DarkMatter_Primary::instance()->get();
 
     if ( $host !== $primary->domain || is_ssl() === $primary->is_https ) {
@@ -72,4 +119,4 @@ function darkmatter_maybe_redirect() {
  * We use "muplugins_loaded" action (introduced in WordPress 2.8.0) rather than
  * the "ms_loaded" (introduced in WordPress 4.6.0).
  */
-add_action( 'muplugins_loaded', 'darkmatter_maybe_redirect' );
+add_action( 'muplugins_loaded', 'darkmatter_maybe_redirect', 20 );
