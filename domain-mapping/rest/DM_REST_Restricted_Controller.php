93e0ac27654e0b4168b0128c9bb44779e3e071ad
Cameron Terry <cammy.wan.kenobi@gmail.com>
2019-01-27 20:05:36 +0000

    Added the outline for the REST controllers.

---
+++ domain-mapping/rest/DM_REST_Restricted_Controller.php
@@ -0,0 +1,55 @@
+<?php
+
+class DM_REST_Restricted_Controller extends WP_REST_Posts_Controller {
+    public function __construct() {
+        $this->namespace = 'darkmatter/restricted';
+    }
+
+    public function create_item( $request ) {
+
+    }
+
+    public function create_item_permissions_check( $request ) {
+
+    }
+
+    public function delete_item( $request ) {
+
+    }
+
+    public function delete_item_permissions_check( $request ) {
+
+    }
+
+    public function get_item( $request ) {
+
+    }
+
+    public function get_items( $request ) {
+
+    }
+
+    public function get_items_permissions_check( $request ) {
+
+    }
+
+    protected function prepare_item_for_database( $request ) {
+
+    }
+
+    public function prepare_item_for_response( $item, $request ) {
+
+    }
+
+    public function register_routes() {
+
+    }
+
+    public function update_item( $request ) {
+
+    }
+
+    public function update_item_permissions_check( $request ) {
+
+    }
+}
