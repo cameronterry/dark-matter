72ad7f098745ce191d6b3e8bebcf63b907985069
Cameron Terry <cammy.wan.kenobi@gmail.com>
2019-02-02 12:48:28 +0000

    Included the Restricted controller.

--- dark-matter.php
+++ dark-matter.php
@@ -44,6 +44,8 @@ require_once DM_PATH . '/domain-mapping/classes/DarkMatter_Domains.php';
 require_once DM_PATH . '/domain-mapping/classes/DarkMatter_Primary.php';
 require_once DM_PATH . '/domain-mapping/classes/DarkMatter_Restrict.php';
 
+require_once DM_PATH . '/domain-mapping/rest/DM_REST_Restricted_Controller.php';
+
 if ( defined( 'WP_CLI' ) && WP_CLI ) {
     require_once DM_PATH . '/domain-mapping/cli/domain.php';
     require_once DM_PATH . '/domain-mapping/cli/restrict.php';
