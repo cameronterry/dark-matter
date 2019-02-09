67461b10d476558efb31c1c5eb65451f60b6a72f
Cameron Terry <cammy.wan.kenobi@gmail.com>
2019-02-09 21:33:18 +0000

    Built an extremely basic DomainRow component.

---
+++ domain-mapping/ui/Components/DomainRow.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class DomainRow extends React.Component {
+  render() {
+    return (
+      <tr>
+        <td>Domain</td>
+        <td>Yes</td>
+        <td>Yes</td>
+        <td>HTTP</td>
+      </tr>
+    );
+  }
+}
+
+export default DomainRow;
