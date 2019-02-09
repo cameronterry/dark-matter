bc85c892cd17293db3624e27b0fdb4be32abecd5
Cameron Terry <cammy.wan.kenobi@gmail.com>
2019-02-09 21:35:37 +0000

    Extended the DomainMapping component to dynamically generate the table of domains.

--- domain-mapping/ui/Components/DomainMapping.js
+++ domain-mapping/ui/Components/DomainMapping.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import DomainRow from './DomainRow';
 
 class DomainMapping extends React.Component {
   render() {
-    return <div>Hello World</div>
+    const rows = [];
+
+    this.props.domains.forEach( ( domain ) => {
+      rows.push( <DomainRow key={domain.id} domain={domain} /> );
+    } );
+
+    return (
+      <div className="wrap">
+        <h1>Domains</h1>
+        <table className="wp-list-table widefat fixed striped users">
+          <thead>
+            <tr>
+              <th scope="col" className="manage-column">Domain</th>
+              <th scope="col" className="manage-column">Is Primary?</th>
+              <th scope="col" className="manage-column">Active?</th>
+              <th scope="col" className="manage-column">Protocol</th>
+            </tr>
+          </thead>
+          <tbody>
+            {rows}
+          </tbody>
+        </table>
+      </div>
+    );
   }
 }
 
