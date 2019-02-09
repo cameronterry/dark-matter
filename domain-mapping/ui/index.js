7cf320861c7e3df5875597aeb2a764e5c878e9d1
Cameron Terry <cammy.wan.kenobi@gmail.com>
2019-02-09 22:18:46 +0000

    Removed the hard-coded test data.

--- domain-mapping/ui/index.js
+++ domain-mapping/ui/index.js
@@ -2,9 +2,4 @@ import React from 'react';
 import { render } from 'react-dom';
 import DomainMapping from './Components/DomainMapping';
 
-const DOMAINS = [
-  { id: 1, domain : 'www.example.com', is_primary: true, active: true, is_https: false },
-  { id: 2, domain : 'www.bbc.co.uk', is_primary: false, active: true, is_https: false }
-];
-
-render( <DomainMapping domains={DOMAINS} />, document.querySelector( '#root' ) );
+render( <DomainMapping />, document.querySelector( '#root' ) );
