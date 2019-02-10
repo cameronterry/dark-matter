7a42b3c5caa9fc711a20f400def9b05a229799b5
Cameron Terry <cammy.wan.kenobi@gmail.com>
2019-02-10 15:53:21 +0000

    Added a comment to explain why Dark Matter includes React.

--- domain-mapping/ui/index.js
+++ domain-mapping/ui/index.js
@@ -2,4 +2,10 @@ import React from 'react';
 import { render } from 'react-dom';
 import DomainMapping from './Components/DomainMapping';
 
+/**
+ * We include React within the code for Dark Matter. WordPress does have React
+ * but it's currently part of wp.element, but this is specifically for Gutenberg
+ * and it's blocks. Which we do not need here.
+ */
+
 render( <DomainMapping />, document.querySelector( '#root' ) );
