0efb7db404c69b285cc44e24c542725d2621adc8
Cameron Terry <cammy.wan.kenobi@gmail.com>
2019-02-10 15:53:05 +0000

    Switched up the DomainRow component to use the two new components.

--- domain-mapping/ui/Components/DomainRow.js
+++ domain-mapping/ui/Components/DomainRow.js
@@ -1,18 +1,22 @@
 import React from 'react';
+import DomainDisplayPrimary from './DomainDisplayPrimary';
+import DomainDisplaySecondary from './DomainDisplaySecondary';
 
 class DomainRow extends React.Component {
+  /**
+   * Render.
+   */
   render() {
     return (
       <tr>
-        <td>
-          <p>
-            <strong>{ this.props.domain.domain }</strong>
-          </p>
-          <a href="#">Set as Primary</a> | <a href="#">Change to HTTPS</a>
-        </td>
-        <td>{ (this.props.domain.is_primary ? 'Yes' : 'No' ) }</td>
-        <td>{ (this.props.domain.active ? 'Yes' : 'No' ) }</td>
-        <td>{ (this.props.domain.is_https ? 'HTTPS' : 'HTTP' ) }</td>
+        { this.props.domain.is_primary ?
+          <DomainDisplayPrimary domain={ this.props.domain.domain } is_https={ this.props.domain.is_https } />
+          :
+          <DomainDisplaySecondary domain={ this.props.domain.domain } is_https={ this.props.domain.is_https } />
+        }
+        <td>{ ( this.props.domain.is_primary ? 'Yes' : 'No' ) }</td>
+        <td>{ ( this.props.domain.active ? 'Yes' : 'No' ) }</td>
+        <td>{ ( this.props.domain.is_https ? 'HTTPS' : 'HTTP' ) }</td>
       </tr>
     );
   }
