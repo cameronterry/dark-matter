597553725c1c3c22ac04d878a9bb3f9a11e0c490
Cameron Terry <cammy.wan.kenobi@gmail.com>
2019-02-16 20:26:33 +0000

    Better fleshed out the Message component.

--- domain-mapping/ui/Components/Message.js
+++ domain-mapping/ui/Components/Message.js
@@ -1,13 +1,27 @@
 import React from 'react';
 
 class Message extends React.Component {
+  handleDismiss = ( event ) => {
+    event.preventDefault();
+
+    this.props.dismiss( this.props.id, this.props.index );
+  }
+
   /**
    * Render.
    */
   render() {
+    let { notice } = this.props;
+    let classes = [ 'notice', 'is-dismissable' ];
+
+    classes.push( 'notice-' + notice.type );
+
     return (
-      <div className="updated notice notice-success is-dismissible">
-        <p>{ this.props.message }</p>
+      <div className={ classes.join( ' ' ) }>
+        <p><strong>{ notice.domain }</strong>; { notice.text }</p>
+        <button className="notice-dismiss" onClick={ this.handleDismiss }>
+          <span className="screen-reader-text">Dismiss this notice.</span>
+        </button>
       </div>
     );
   }
