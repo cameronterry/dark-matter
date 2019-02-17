27540c97b719b4d78bd5a0b13bd0c39691f46128
Cameron Terry <cammy.wan.kenobi@gmail.com>
2019-02-17 10:43:12 +0000

    Moved the handle methods from DomainDisplay components to DomainRow component.

--- domain-mapping/ui/Components/DomainDisplaySecondary.js
+++ domain-mapping/ui/Components/DomainDisplaySecondary.js
@@ -2,65 +2,6 @@ import React from 'react';
 
 class DomainDisplaySecondary extends React.Component {
   /**
-   * Handle the Deleting of the domain.
-   */
-  handleDelete = ( event ) => {
-    event.preventDefault();
-
-    let confirm = window.confirm( 'Are you sure you wish to delete ' + this.props.data.domain + '?' );
-
-    if ( ! confirm ) {
-      return;
-    }
-
-    this.props.delete( this.props.data.domain );
-  }
-
-  /**
-   * Handle the Setting the primary domain.
-   */
-  handlePrimary = ( event ) => {
-    event.preventDefault();
-
-    let confirm = window.confirm( 'Are you sure you wish to change ' + this.props.data.domain + ' to be the primary domain? This will cause 301 redirects and may affect SEO.' );
-
-    if ( ! confirm ) {
-      return;
-    }
-
-    let data = { ...this.props.data };
-    data.is_primary = true;
-
-    this.props.update( data );
-  }
-
-  /**
-   * Handle the change in Protocol.
-   */
-  handleProtocol = ( event ) => {
-    event.preventDefault();
-
-    let data = { ...this.props.data };
-    let value = ! data.is_https;
-
-    /**
-     * We only want to get confirmation from the user if we are changing to
-     * HTTPS.
-     */
-    if ( value ) {
-      let confirm = window.confirm( 'Please ensure that your server configuration is setup properly before changing ' + this.props.data.domain + ' to be HTTPS. Do you wish to proceed?' );
-
-      if ( ! confirm ) {
-        return;
-      }
-    }
-
-    data.is_https = value;
-
-    this.props.update( data );
-  }
-
-  /**
    * Render component.
    */
   render() {
@@ -71,11 +12,11 @@ class DomainDisplaySecondary extends React.Component {
         <p>
           <a href={ url }>{ this.props.data.domain }</a>
         </p>
-        <a href="#" onClick={ this.handlePrimary }>Set as Primary</a>
+        <a href="#" onClick={ this.props.primary }>Set as Primary</a>
         <span>|</span>
-        <a href="#" onClick={ this.handleProtocol }>Change to { this.props.data.is_https ? 'HTTP' : 'HTTPS' }</a>
+        <a href="#" onClick={ this.props.protocol }>Change to { this.props.data.is_https ? 'HTTP' : 'HTTPS' }</a>
         <span>|</span>
-        <a href="#" className="submitdelete" onClick={ this.handleDelete }>Delete</a>
+        <a href="#" className="submitdelete" onClick={ this.props.delete }>Delete</a>
       </td>
     );
   }
--- domain-mapping/ui/Components/DomainRow.js
+++ domain-mapping/ui/Components/DomainRow.js
@@ -4,15 +4,74 @@ import DomainDisplaySecondary from './DomainDisplaySecondary';
 
 class DomainRow extends React.Component {
   /**
+   * Handle the Deleting of the domain.
+   */
+  handleDelete = ( event ) => {
+    event.preventDefault();
+
+    let confirm = window.confirm( 'Are you sure you wish to delete ' + this.props.domain.domain + '?' );
+
+    if ( ! confirm ) {
+      return;
+    }
+
+    this.props.delete( this.props.domain.domain );
+  }
+
+  /**
+   * Handle the Setting the primary domain.
+   */
+  handlePrimary = ( event ) => {
+    event.preventDefault();
+
+    let confirm = window.confirm( 'Are you sure you wish to change ' + this.props.domain.domain + ' to be the primary domain? This will cause 301 redirects and may affect SEO.' );
+
+    if ( ! confirm ) {
+      return;
+    }
+
+    let data = { ...this.props.domain };
+    data.is_primary = true;
+
+    this.props.update( data );
+  }
+
+  /**
+   * Handle the change in Protocol.
+   */
+  handleProtocol = ( event ) => {
+    event.preventDefault();
+
+    let data = { ...this.props.domain };
+    let value = ! data.is_https;
+
+    /**
+     * We only want to get confirmation from the user if we are changing to
+     * HTTPS.
+     */
+    if ( value ) {
+      let confirm = window.confirm( 'Please ensure that your server configuration is setup properly before changing ' + this.props.domain.domain + ' to be HTTPS. Do you wish to proceed?' );
+
+      if ( ! confirm ) {
+        return;
+      }
+    }
+
+    data.is_https = value;
+
+    this.props.update( data );
+  }
+
+  /**
    * Render.
    */
   render() {
     return (
       <tr>
         { this.props.domain.is_primary ?
-          <DomainDisplayPrimary domain={ this.props.domain.domain } is_https={ this.props.domain.is_https } />
+          <DomainDisplayPrimary data={ this.props.domain } protocol={ this.handleProtocol } delete={ this.props.handleDelete } />
           :
-          <DomainDisplaySecondary data={ this.props.domain } update={ this.props.update } delete={ this.props.delete } />
+          <DomainDisplaySecondary data={ this.props.domain } primary={ this.handlePrimary } protocol={ this.handleProtocol } delete={ this.handleDelete } />
         }
         <td>{ ( this.props.domain.is_primary ? 'Yes' : 'No' ) }</td>
         <td>{ ( this.props.domain.active ? 'Yes' : 'No' ) }</td>
