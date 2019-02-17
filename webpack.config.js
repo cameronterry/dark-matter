367131a1ddec502a719602a94f69adbeacc49fdc
Cameron Terry <cammy.wan.kenobi@gmail.com>
2019-02-17 16:22:05 +0000

    Updated webpack config to provide production and development builds.

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,18 @@
 // node module that let's us do file system stuffs...
 const path = require( 'path' );
+const UglifyJsPlugin = require( 'uglifyjs-webpack-plugin' );
 const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
+const OptimizeCSSAssetsPlugin = require( 'optimize-css-assets-webpack-plugin' );
 
 // Webpack expects an exported object with all the configurations, so we export an object here
-module.exports = [
-  {
+module.exports = ( env, argv ) => {
+  let config = {
     name: 'domain-mapping',
     entry: './domain-mapping/ui/index.js', // Where to find our main js
     output: {
       // where we want our built file to go to and be named
       // I name it index.build.js so I keep index files separate
-      filename: 'index.js',
+      filename: 'build' + ( 'production' === argv.mode ? '.min' : '' ) + '.js',
       // we're going to put our built file in a './build/' folder
       path: path.resolve( __dirname, 'domain-mapping/build' )
     },
@@ -45,12 +47,26 @@ module.exports = [
         }
       ]
     },
-    // Webpack yells at you if you don't choose a mode...
-    mode: 'development',
+    mode: argv.mode,
     plugins: [
       new MiniCssExtractPlugin( {
-        filename: 'style.css'
+        filename: 'style' + ( 'production' === argv.mode ? '.min' : '' ) + '.css'
       } )
     ]
+  };
+
+  if ( 'production' === argv.mode ) {
+    config.optimization = {
+      minimizer: [
+        new UglifyJsPlugin( {
+          cache: true,
+          parallel: true,
+          sourceMap: true // set to true if you want JS source maps
+        } ),
+        new OptimizeCSSAssetsPlugin( {} )
+      ]
+    };
   }
-];
+
+  return config;
+};
