b0cb25009763f9471126a4fe302152f43d26f491
Cameron Terry <cammy.wan.kenobi@gmail.com>
2019-02-17 10:16:46 +0000

    Added extremely basic confirmation dialogs to the update / delete actions.

--- domain-mapping/ui/Components/DomainDisplaySecondary.js
+++ domain-mapping/ui/Components/DomainDisplaySecondary.js
@@ -7,11 +7,26 @@ class DomainDisplaySecondary extends React.Component {
   handleDelete = ( event ) => {
     event.preventDefault();
 
+    let confirm = window.confirm( 'Are you sure you wish to delete ' + this.props.data.domain + '?' );
+
+    if ( ! confirm ) {
+      return;
+    }
+
     this.props.delete( this.props.data.domain );
   }
 
+  /**
+   * Handle the Setting the primary domain.
+   */
   handlePrimary = ( event ) => {
     event.preventDefault();
+
+    let confirm = window.confirm( 'Are you sure you wish to change ' + this.props.data.domain + ' to be the primary domain? This will cause 301 redirects and may affect SEO.' );
+
+    if ( ! confirm ) {
+      return;
+    }
 
     let data = { ...this.props.data };
     data.is_primary = true;
@@ -22,11 +37,25 @@ class DomainDisplaySecondary extends React.Component {
   /**
    * Handle the change in Protocol.
    */
-  handleProtocol = () => {
+  handleProtocol = ( event ) => {
     event.preventDefault();
 
+    let value = ! data.is_https;
+
+    /**
+     * We only want to get confirmation from the user if we are changing to
+     * HTTPS.
+     */
+    if ( value ) {
+      let confirm = window.confirm( 'Please ensure that your server configuration is setup properly before changing ' + this.props.data.domain + ' to be HTTPS. Do you wish to proceed?' );
+
+      if ( ! confirm ) {
+        return;
+      }
+    }
+
     let data = { ...this.props.data };
-    data.is_https = ! data.is_https;
+    data.is_https = value;
 
     this.props.update( data );
   }
