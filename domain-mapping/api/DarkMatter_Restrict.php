86b22834472be90991ffbb919a07467e81e3e1a1
Cameron Terry <cammy.wan.kenobi@gmail.com>
2019-02-23 11:22:10 +0000

    Added a action for when a restricted domain is added.

--- domain-mapping/api/DarkMatter_Restrict.php
+++ domain-mapping/api/DarkMatter_Restrict.php
@@ -88,6 +88,15 @@ class DarkMatter_Restrict {
 
         $this->refresh_cache();
 
+        /**
+         * Fires when a domain is added to the restricted list.
+         *
+         * @since 2.0.0
+         *
+         * @param string $fqdn Domain name that was restricted.
+         */
+        do_action( 'darkmatter_restrict_add', $fqdn );
+
         return true;
     }
 
