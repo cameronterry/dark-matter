09a237545960fadb4a8dce6b3cd0cdedb030105a
Cameron Terry <cammy.wan.kenobi@gmail.com>
2019-03-03 15:11:35 +0000

    Added the update method to the Dropin CLI.

--- domain-mapping/cli/update.php
+++ domain-mapping/cli/update.php
@@ -3,5 +3,16 @@
 defined( 'ABSPATH' ) || die;
 
 class DarkMatter_Dropin_CLI {
+    /**
+     * Upgrade the Sunrise dropin plugin to the latest version within the Dark
+     * Matter plugin.
+     *
+     * ### EXAMPLES
+     * Update the sunrise.php dropin plugin.
+     *
+     *      wp darkmatter dropin update
+     */
+    public function update( $args, $assoc_args ) {
+    }
 }
 WP_CLI::add_command( 'darkmatter dropin', 'DarkMatter_Dropin_CLI' );
