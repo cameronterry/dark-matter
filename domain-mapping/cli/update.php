9d43c2cbebd9eecf233e830157a7ffb75a90fbf1
Cameron Terry <cammy.wan.kenobi@gmail.com>
2019-03-03 15:20:25 +0000

    Added scope for a --force flag on the dropin update CLI.

--- domain-mapping/cli/update.php
+++ domain-mapping/cli/update.php
@@ -7,10 +7,20 @@ class DarkMatter_Dropin_CLI {
      * Upgrade the Sunrise dropin plugin to the latest version within the Dark
      * Matter plugin.
      *
+     * ### OPTIONS
+     *
+     * [--force]
+     * : Force Dark Matter to override and update Sunrise dropin if a file
+     * already exists.
+     *
      * ### EXAMPLES
-     * Update the sunrise.php dropin plugin.
+     * Install the Sunrise dropin plugin for new installations.
      *
      *      wp darkmatter dropin update
+     *
+     * Update the Sunrise dropin plugin, even if a file is already present.
+     *
+     *      wp darkmatter dropin update --force
      */
     public function update( $args, $assoc_args ) {
         $destination = WP_CONTENT_DIR . '/sunrise.php';
