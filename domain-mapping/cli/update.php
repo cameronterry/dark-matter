c09aa249f65743c619678dd5f390ae07632b7466
Cameron Terry <cammy.wan.kenobi@gmail.com>
2019-03-03 15:20:42 +0000

    Implemented the various checks for the dropin update CLI.

--- domain-mapping/cli/update.php
+++ domain-mapping/cli/update.php
@@ -25,6 +25,32 @@ class DarkMatter_Dropin_CLI {
     public function update( $args, $assoc_args ) {
         $destination = WP_CONTENT_DIR . '/sunrise.php';
         $source      = DM_PATH . '/domain-mapping/sunrise.php';
+
+        $opts = wp_parse_args( $assoc_args, [
+            'force'   => false,
+        ] );
+
+        if ( false === is_writable( WP_CONTENT_DIR ) ) {
+            WP_CLI::error( __( 'The /wp-content/ directory needs to be writable by the current user in order to update.', 'dark-matter' ) );
+        }
+
+        if ( file_exists( $destination ) && false === $opts['force'] ) {
+            WP_CLI::error( __( 'Sunrise is already present. Use the --force flag to override.', 'dark-matter' ) );
+        }
+
+        if ( false === is_readable( $source ) ) {
+            WP_CLI::error( __( 'Cannot read the Sunrise dropin within the Dark Matter plugin folder.', 'dark-matter' ) );
+        }
+
+        if ( false === file_exists( $source ) ) {
+            WP_CLI::error( __( 'Sunrise dropin within the Dark Matter plugin is missing.', 'dark-matter' ) );
+        }
+
+        if ( @copy( $source, $destination ) ) {
+            WP_CLI::success( __( 'Updated the Sunrise dropin to the latest version.', 'dark-matter' ) );
+        } else {
+            WP_CLI::error( __( 'Unknown error occurred preventing the update of Sunrise dropin.', 'dark-matter' ) );
+        }
     }
 }
 WP_CLI::add_command( 'darkmatter dropin', 'DarkMatter_Dropin_CLI' );
