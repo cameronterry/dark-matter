df11ddb8a47d0e67be2e674d4256387e5f78877a
Cameron Terry <cammy.wan.kenobi@gmail.com>
2019-05-20 19:23:04 +0100

    Only attempt advanced-cache.php if the request method is GET or HEAD.

--- advanced-cache/inc/advanced-cache.php
+++ advanced-cache/inc/advanced-cache.php
@@ -1,6 +1,13 @@
 <?php
 defined( 'ABSPATH' ) || die;
 
+/**
+ * Only cache requests which are GET or HEAD.
+ */
+if ( ! empty( $_SERVER['REQUEST_METHOD'] ) && ! in_array( $_SERVER['REQUEST_METHOD'], array( 'GET', 'HEAD' ) ) ) {
+    return;
+}
+
 class DM_Advanced_Cache {
     /**
      * Constructor
