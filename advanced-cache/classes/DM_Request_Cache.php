63882bffe307ca207299527d3866408d96b7a5b2
Cameron Terry <cammy.wan.kenobi@gmail.com>
2019-08-31 11:41:15 +0100

    Added an outline for the DM_Request_Cache class.

--- advanced-cache/classes/DM_Request_Cache.php
+++ advanced-cache/classes/DM_Request_Cache.php
@@ -2,6 +2,41 @@
 defined( 'ABSPATH' ) || die;
 
 class DM_Request_Cache {
-    public function __construct() {
+    /**
+     * @var string Cache Key for the URL - minus any variants - of the Request.
+     */
+    private $url_cache_key = '';
+
+    /**
+     * DM_Request_Cache constructor.
+     *
+     * @param string $url URL to retrieve the Request Cache Entry.
+     */
+    public function __construct( $url = '' ) {
+    }
+
+    /**
+     * Delete the Request Cache Entry.
+     */
+    public function delete() {
+
+    }
+
+    /**
+     * Retrieve the Request Cache Entry - if available - and return it.
+     */
+    public function get() {
+
+    }
+
+    /**
+     * Store the generate HTML in cache.
+     *
+     * @param string $output HTML to be added to the Request Cache entry.
+     */
+    public function set( $output = '' ) {
+        
+
+        return md5( $host . '/' . $path );
     }
 }
