9bf620cfc96a3912cb989089cdc552105fe9debf
Cameron Terry <cammy.wan.kenobi@gmail.com>
2019-08-31 10:26:46 +0100

    Added the logic needed to add the DM_Advanced_Cache file where required.

--- advanced-cache/inc/advanced-cache.php
+++ advanced-cache/inc/advanced-cache.php
@@ -8,5 +8,10 @@ if ( ! empty( $_SERVER['REQUEST_METHOD'] ) && ! in_array( $_SERVER['REQUEST_METH
     return;
 }
 
+/**
+ * Cannot utilise plugin_dir_path() as the inner function used is not available and this is preferable to include more
+ * files than is realistically needed.
+ */
+$dirname = str_replace( '/inc', '', dirname( __FILE__ ) );
 
-DM_Advanced_Cache::instance();
+require_once $dirname . '/classes/DM_Advanced_Cache.php';
