bd2832427d9bf55270b73a519a8c9cb70d919ef1
Cameron Terry <cammy.wan.kenobi@gmail.com>
2019-08-31 11:43:12 +0100

    Added the logic to get the base URL key.

--- advanced-cache/classes/DM_Request_Cache.php
+++ advanced-cache/classes/DM_Request_Cache.php
@@ -13,6 +13,7 @@ class DM_Request_Cache {
      * @param string $url URL to retrieve the Request Cache Entry.
      */
     public function __construct( $url = '' ) {
+        $this->url_cache_key = $this->get_url_key();
     }
 
     /**
@@ -30,13 +31,23 @@ class DM_Request_Cache {
     }
 
     /**
+     * Generates a URL Key for the Request. This can be used to retrieve
+     *
+     * @return string MD5 hash key.
+     */
+    public function get_url_key() {
+        $host = rtrim( trim( $_SERVER['HTTP_HOST'] ), '/' );
+        $path = trim( strtok( $_SERVER['REQUEST_URI'], '?' ) );
+
+        return md5( $host . '/' . $path );
+    }
+
+    /**
      * Store the generate HTML in cache.
      *
      * @param string $output HTML to be added to the Request Cache entry.
      */
     public function set( $output = '' ) {
-        
 
-        return md5( $host . '/' . $path );
     }
 }
