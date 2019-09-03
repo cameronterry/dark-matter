57f483a2a105c9600c79313eb9788b0430c12b44
Cameron Terry <cammy.wan.kenobi@gmail.com>
2019-09-03 21:18:17 +0100

    Added methods to add / remove variants on the Request Data Record.

--- advanced-cache/classes/DM_Request_Data.php
+++ advanced-cache/classes/DM_Request_Data.php
@@ -8,11 +8,40 @@ class DM_Request_Data {
     private $cache_key = '';
 
     /**
+     * @var array
+     */
+    private $variants = [];
+
+    /**
      * DM_Request_Data constructor.
      *
      * @param string $base_url URL to retrieve Cache data for.
      */
     public function __construct( $base_url = '' ) {
         $this->cache_key = md5( $base_url );
+    }
+
+    /**
+     * Add a variant to the Request Cache Data record.
+     *
+     * @param string $variant_key Variant key to be added.
+     */
+    public function variant_add( $variant_key = '' ) {
+        if ( ! in_array( $variant_key, $this->variants, true ) ) {
+            $this->variants[] = $variant_key;
+        }
+    }
+
+    /**
+     * Remove a variant to the Request Cache Data record.
+     *
+     * @param string $variant_key Variant key to be removed.
+     */
+    public function variant_remove( $variant_key = '' ) {
+        $pos = array_search( $variant_key, $this->variants, true );
+
+        if ( false !== $pos ) {
+            unset( $this->variants[ $pos ] );
+        }
     }
 }
