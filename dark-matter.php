0c7e1ba56b32920504cce505e7d99e3e1e0c547c
Cameron Terry <cammy.wan.kenobi@gmail.com>
2019-09-07 18:35:04 +0100

    Altered the line break point for comment lines on the root plugin file to be more consistent with the rest (PhpStorm default).

--- dark-matter.php
+++ dark-matter.php
@@ -12,18 +12,13 @@
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  * GitHub Plugin URI: https://github.com/cameronterry/dark-matter
  *
- * Dark Matter is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * any later version.
+ * Dark Matter is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 2 of the License, or any later version.
  *
- * Dark Matter is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * Dark Matter is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with Dark Matter. If not, see
+ * You should have received a copy of the GNU General Public License along with Dark Matter. If not, see
  * https://github.com/cameronterry/dark-matter/blob/master/license.txt.
  */
 
