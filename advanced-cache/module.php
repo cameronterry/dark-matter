b244e35062189d67306fea9be18505ac4cf101dd
Cameron Terry <cammy.wan.kenobi@gmail.com>
2019-09-07 19:29:48 +0100

    Ensured that the fullpage cache global group is available.

--- advanced-cache/module.php
+++ advanced-cache/module.php
@@ -1,5 +1,7 @@
 <?php
 defined( 'ABSPATH' ) || die;
 
+wp_cache_add_global_groups( 'dark-matter-fullpage-data' );
+
 require_once DM_PATH . '/advanced-cache/DM_Request_Data.php';
 require_once DM_PATH . '/advanced-cache/DM_Save_Post.php';
