81fed333d98680cf6e857a70067d914f98273e5b
Cameron Terry <cammy.wan.kenobi@gmail.com>
2019-09-08 13:23:24 +0100

    Made sure the dark-matter-fullpage cache group is available for use for WP Admin.

--- advanced-cache/module.php
+++ advanced-cache/module.php
@@ -1,6 +1,7 @@
 <?php
 defined( 'ABSPATH' ) || die;
 
+wp_cache_add_global_groups( 'dark-matter-fullpage' );
 wp_cache_add_global_groups( 'dark-matter-fullpage-data' );
 
 require_once DM_PATH . 'advanced-cache/classes/DM_Cache_Post.php';
