e7d5aed11a2bcd6389d270e9cd11e22bbfbac05d
Cameron Terry <cammy.wan.kenobi@gmail.com>
2020-01-05 19:21:23 +0000

    Added an optional config file include for advanced cache.

--- advanced-cache/advanced-cache.php
+++ advanced-cache/advanced-cache.php
@@ -11,6 +11,18 @@ if ( ! defined( 'DARKMATTER_FULLPAGECACHE' ) ) {
     define( 'DARKMATTER_FULLPAGECACHE', true );
 }
 
+/**
+ * Allow for the inclusion of third party logic which can utilise the hooks within Advanced Cache.
+ */
+$cache_config_path = WP_CONTENT_DIR . '/advanced-cache-config.php';
+
+if ( is_readable( $cache_config_path ) ) {
+    require_once( $cache_config_path );
+}
+
+/**
+ * Load the Advanced Cache logic to perform the full page cache.
+ */
 $cache_path = ( dirname( __FILE__ ) . '/plugins/dark-matter/advanced-cache/inc/advanced-cache.php' );
 
 if ( is_readable( $cache_path ) ) {
