c28f021e16313abf3de37bed828f28eb52c0a618
Cameron Terry <cammy.wan.kenobi@gmail.com>
2020-01-11 13:57:29 +0000

    Added the logic for constructing Cache Info data set.

--- advanced-cache/classes/DM_Cache_Info.php
+++ advanced-cache/classes/DM_Cache_Info.php
@@ -30,6 +30,44 @@ class DM_Cache_Info {
     public function __construct( $url = '' ) {
         $this->cache_entry = new DM_Request_Cache( $url );
         $this->cache_data  = $this->cache_entry->get_data()->data();
+
+        $this->compile_data();
+    }
+
+    /**
+     * Construct the overall data record.
+     */
+    private function compile_data() {
+        if ( empty( $this->cache_data ) ) {
+            return '';
+        }
+
+        foreach ( $this->cache_data['variants'] as $variant_key => $variant_data ) {
+            $time        = $variant_data['time_utc'];
+            $expiry_time = $time + $variant_data['ttl_secs'];
+            $remaining   = human_time_diff( time(), $expiry_time );
+
+            if ( time() > $expiry_time ) {
+                $remaining = __( 'Expired', 'dark-matter' );
+            }
+
+            /**
+             * Convert the Unix timestamp to a human readable format and in the website's current timezone.
+             */
+            $datetime = new DateTime( "@{$time}" );
+            $datetime->setTimezone( wp_timezone() );
+
+            $this->data[] = [
+                'Variant Key'  => $variant_key,
+                'Variant Name' => $variant_data['name'],
+                'Provider'     => $this->cache_data['provider'],
+                'Time'         => $datetime->format( 'r' ),
+                'Remaining'    => $remaining,
+                'TTL'          => human_time_diff( $time, $expiry_time ),
+                'Size'         => size_format( $variant_data['size_bytes'] ),
+                'Headers'      => $variant_data['headers'],
+            ];
+        }
     }
 
     /**
