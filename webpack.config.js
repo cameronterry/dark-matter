2637f90c78c8d017a4d3e4af0d3c2ed10ff1793d
Cameron Terry <cammy.wan.kenobi@gmail.com>
2020-05-16 16:09:53 +0100

    Added Terser and updated the Webpack configuration.

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 // node module that let's us do file system stuffs...
 const path = require( 'path' );
-const UglifyJsPlugin = require( 'uglifyjs-webpack-plugin' );
-const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
-const OptimizeCSSAssetsPlugin = require( 'optimize-css-assets-webpack-plugin' );
+const TerserPlugin = require('terser-webpack-plugin'); // eslint-disable-line import/no-extraneous-dependencies
 const WebpackBar = require('webpackbar');
 
 // Webpack expects an exported object with all the configurations, so we export an object here
@@ -44,9 +42,6 @@ module.exports = ( env, argv ) => {
     },
     mode: argv.mode,
     plugins: [
-      new MiniCssExtractPlugin( {
-        filename: 'style' + ( 'production' === argv.mode ? '.min' : '' ) + '.css'
-      } ),
       new WebpackBar(),
     ]
   };
@@ -54,12 +49,46 @@ module.exports = ( env, argv ) => {
   if ( 'production' === argv.mode ) {
     config.optimization = {
       minimizer: [
-        new UglifyJsPlugin( {
+        new TerserPlugin( {
           cache: true,
           parallel: true,
-          sourceMap: true // set to true if you want JS source maps
+          sourceMap: false,
+          terserOptions: {
+            parse: {
+              /**
+               * We want terser to parse ecma 8 code. However, we don't want it
+               * to apply any minfication steps that turns valid ecma 5 code
+               * into invalid ecma 5 code. This is why the 'compress' and 'output'
+               * sections only apply transformations that are ecma 5 safe
+               * https://github.com/facebook/create-react-app/pull/4234
+               */
+              ecma: 8,
+            },
+            compress: {
+              ecma: 5,
+              warnings: false,
+              /**
+               * Disabled because of an issue with Uglify breaking seemingly valid code:
+               * https://github.com/facebook/create-react-app/issues/2376
+               * Pending further investigation:
+               * https://github.com/mishoo/UglifyJS2/issues/2011
+               */
+              comparisons: false,
+              /**
+               * Disabled because of an issue with Terser breaking valid code:
+               * https://github.com/facebook/create-react-app/issues/5250
+               * Pending futher investigation:
+               * https://github.com/terser-js/terser/issues/120
+               */
+              inline: 2,
+            },
+            output: {
+              ecma: 5,
+              comments: false,
+            },
+            ie8: false,
+          },
         } ),
-        new OptimizeCSSAssetsPlugin( {} )
       ]
     };
   }
