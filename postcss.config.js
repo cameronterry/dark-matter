022c869dbfda8b06e9aadd60bad2396967aeb3eb
Cameron Terry <cammy.wan.kenobi@gmail.com>
2020-05-17 16:22:23 +0100

    Configured PostCSS to not preserve Custom Properties as well as the autoprefixer.

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,11 @@ module.exports = ( { env } ) => ( {
   plugins: {
     'postcss-import': {},
     'postcss-preset-env': {
+      autoprefixer: {
+        grid: true
+      },
+      browsers: 'last 2 versions',
+      preserve: false,
       stage: 0
     },
     'cssnano':
