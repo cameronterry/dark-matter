e75d6481cc34d0355f617d45648f624de35727b6
Cameron Terry <cammy.wan.kenobi@gmail.com>
2020-05-23 18:46:10 +0100

    Switched the domain actions from links to buttons.

--- domain-mapping/ui/Components/DomainDisplaySecondary.js
+++ domain-mapping/ui/Components/DomainDisplaySecondary.js
@@ -1,31 +1,37 @@
 import React from 'react';
 
 class DomainDisplaySecondary extends React.Component {
-  /**
-   * Render component.
-   */
-  render() {
-    const url = ( this.props.data.is_https ? 'https://' : 'http://' ) + this.props.data.domain;
+	/**
+	 * Render component.
+	 */
+	render() {
+		const url =
+			( this.props.data.is_https ? 'https://' : 'http://' ) +
+			this.props.data.domain;
 
-    return (
-      <td className="domain-options">
-        <p>
-          <a href={ url }>{ this.props.data.domain }</a>
-        </p>
-        { this.props.data.is_active ?
-          <a href="#" onClick={ this.props.activate }>Deactivate</a>
-            :
-            <a href="#" onClick={ this.props.activate }>Activate</a>
-        }
-        <span>|</span>
-        <a href="#" onClick={ this.props.primary }>Set as Primary</a>
-        <span>|</span>
-        <a href="#" onClick={ this.props.protocol }>Change to { this.props.data.is_https ? 'HTTP' : 'HTTPS' }</a>
-        <span>|</span>
-        <a href="#" className="submitdelete" onClick={ this.props.delete }>Delete</a>
-      </td>
-    );
-  }
+		return (
+			<td className="domain-options">
+				<p>
+					<a href={ url }>{ this.props.data.domain }</a>
+				</p>
+				{ this.props.data.is_active ? (
+					<button onClick={ this.props.activate }>Deactivate</button>
+				) : (
+					<button onClick={ this.props.activate }>Activate</button>
+				) }
+				<span>|</span>
+				<button onClick={ this.props.primary }>Set as Primary</button>
+				<span>|</span>
+				<button onClick={ this.props.protocol }>
+					Change to { this.props.data.is_https ? 'HTTP' : 'HTTPS' }
+				</button>
+				<span>|</span>
+				<button className="submitdelete" onClick={ this.props.delete }>
+					Delete
+				</button>
+			</td>
+		);
+	}
 }
 
 export default DomainDisplaySecondary;
