fd8e6ff96a50ece207354614a939ce005ce474c2
Cameron Terry <cammy.wan.kenobi@gmail.com>
2020-05-24 22:11:40 +0100

    Updated the release script to work with 2.1.0 changes.

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,22 +7,23 @@ mkdir -p ~/dm/release
 
 rsync -av -progress ./ ~/dm/release --exclude node_modules --exclude vendor
 
-# Run the NPM builds.
+# Run the build.
 cd ~/dm/release
 
-npm install
-
-npm run dev
-npm run build
+npm run release
 
 # Remove the unnecessary files.
 
 rm -rf node_modules
+rm -rf vendor
 rm -rf scripts
 rm -rf domain-mapping/ui
 rm -rf .*
 rm *.json
+rm *.lock
+rm phpcs.xml
+rm postcss.config.js
 rm webpack.config.js
 
-zip -r dark-matter *
-mv dark-matter.zip ../dark-matter.zip
+zip -r dark-matter-$1.zip *
+mv dark-matter-$1.zip ../dark-matter-$1.zip
