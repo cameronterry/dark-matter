04fdcd63e153319afcf9f94b9634f06ef8676149
Cameron Terry <cammy.wan.kenobi@gmail.com>
2020-05-24 12:47:30 +0100

    Ensure the SSO does not load if the COOKIE_DOMAIN is set.

--- dark-matter.php
+++ dark-matter.php
@@ -59,7 +59,7 @@ require_once DM_PATH . '/domain-mapping/api/class-darkmatter-restrict.php';
 /**
  * Disable SSO if the COOKIE_DOMAIN constant is set.
  */
-if ( ! defined( 'COOKIE_DOMAIN' ) && ( ! defined( 'DARKMATTER_SSO_TYPE' ) || 'disable' !== DARKMATTER_SSO_TYPE ) ) {
+if ( DM_HealthChecks::instance()->cookie_domain_dm_set() && ( ! defined( 'DARKMATTER_SSO_TYPE' ) || 'disable' !== DARKMATTER_SSO_TYPE ) ) {
     require_once DM_PATH . '/domain-mapping/sso/class-dm-sso-cookie.php';
 }
 
@@ -68,6 +68,6 @@ require_once DM_PATH . '/domain-mapping/rest/class-dm-rest-restricted-controller
 
 if ( defined( 'WP_CLI' ) && WP_CLI ) {
     require_once DM_PATH . '/domain-mapping/cli/class-darkmatter-domain-cli.php';
+    require_once DM_PATH . '/domain-mapping/cli/class-darkmatter-dropin-cli.php';
     require_once DM_PATH . '/domain-mapping/cli/class-darkmatter-restrict-cli.php';
-    require_once DM_PATH . '/domain-mapping/cli/class-darkmatter-update-cli.php';
 }
