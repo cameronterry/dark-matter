5b30020b74bbd8db9b520a6f4a39d1b4c89b3f88
Cameron Terry <cameronterry@protonmail.com>
2022-04-15 15:50:56 +0100

    Included the stub for the AdvancedCache password.

---
+++ includes/classes/AdvancedCache.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Advanced Cache instantiation and loader.
+ *
+ * @since 3.0.0
+ */
+namespace DarkMatter;
+
+/**
+ * Class AdvancedCache
+ */
+class AdvancedCache implements Registerable {
+
+	/**
+	 * @inerhitDoc
+	 */
+	public function register() {
+
+	}
+}
