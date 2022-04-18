2ed078d7b5f0709678f931311d8d44c8033aee47
Cameron Terry <cameronterry@protonmail.com>
2022-04-18 16:27:08 +0100

    Added logic to get the Status Header from WordPress.

--- includes/classes/AdvancedCache/Data/Response.php
+++ includes/classes/AdvancedCache/Data/Response.php
@@ -5,10 +5,12 @@
 
 namespace DarkMatter\AdvancedCache\Data;
 
+use DarkMatter\Interfaces\Registerable;
+
 /**
  * Class Response
  */
-class Response {
+class Response implements Registerable {
 	/**
 	 * Body as string. This is most commonly HTML but can also be JSON, XML (i.e. RSS), or plaintext.
 	 *
@@ -79,5 +81,30 @@ class Response {
 		$entry->headers = $this->headers;
 
 		return $entry->save();
+	}
+
+	/**
+	 * Register hooks.
+	 *
+	 * Note: used for processing a live request within WordPress. Can be ignored otherwise.
+	 *
+	 * @return void
+	 */
+	public function register() {
+		add_filter( 'status_header', [ $this, 'set_status_header' ], 10, 2 );
+	}
+
+	/**
+	 * Set the status header from the WordPress hook.
+	 *
+	 * Note: used for processing a live request within WordPress. Can be ignored otherwise.
+	 *
+	 * @param string  $status_header HTTP status header.
+	 * @param integer $status_code   HTTP status code.
+	 * @return string
+	 */
+	public function set_status_header( $status_header = '', $status_code = 200 ) {
+		$this->status_code = $status_code;
+		return $status_header;
 	}
 }
