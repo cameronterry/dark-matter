02070782da88d937b4f4cf8349b1f157899c5fef
Cameron Terry <cameronterry@protonmail.com>
2022-05-07 19:11:27 +0100

    Created the initial version of the Request data object.

---
+++ includes/classes/AdvancedCache/Data/Request.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * Request
+ *
+ * @package DarkMatter\AdvancedCache
+ */
+namespace DarkMatter\AdvancedCache\Data;
+
+use DarkMatter\Interfaces\Storeable;
+
+/**
+ * Class Request
+ */
+class Request implements Storeable {
+	/**
+	 * @var string
+	 */
+	public $cache_key = '';
+
+	/**
+	 * Request data, usually provided by the CMS.
+	 *
+	 * @var array
+	 */
+	public $data = [];
+
+	/**
+	 * Expiry as a Unix epoch. Defaults to -1, which equates to "infinite" / "until cleared".
+	 *
+	 * @var int
+	 */
+	public $expiry = -1;
+
+	/**
+	 * Note on the latest instruction received and processed.
+	 *
+	 * @var int
+	 */
+	public $instruction = 0;
+
+	/**
+	 * @var array
+	 */
+	public $variants = [];
+
+	/**
+	 * Constructor.
+	 *
+	 * @param string $full_url Full URL.
+	 */
+	public function __construct( $full_url = '' ) {
+		$this->cache_key = md5( $full_url );
+
+		/**
+		 * Retrieve from cache if possible.
+		 */
+		$cache = wp_cache_get( $this->cache_key, 'dark-matter-fpc-request' );
+		if ( ! empty( $cache ) ) {
+			$this->from_json( $cache );
+		}
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function to_json() {
+		return wp_json_encode(
+			[
+				'data'        => $this->data,
+				'expiry'      => $this->expiry,
+				'instruction' => $this->instruction,
+				'variants'    => $this->variants,
+			]
+		);
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function from_json( $json = '' ) {
+		if ( empty( $json ) ) {
+			return;
+		}
+
+		$obj = json_decode( $json );
+
+		$this->data        = $obj->data;
+		$this->expiry      = $obj->expiry;
+		$this->instruction = $obj->instruction;
+		$this->variants    = $obj->variants;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function save() {
+		return wp_cache_set( $this->cache_key, $this->to_json(), 'dark-matter-fpc-request' );
+	}
+}
