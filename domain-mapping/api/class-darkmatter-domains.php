e3e12d31c77876bc2ea0233a6cbe00388eb73715
Cameron Terry <cameronterry@protonmail.com>
2022-05-14 11:01:02 +0100

    Forgot - when the domain is initially retrieved, it's an array - not stdClass nor DM_Domain.

--- domain-mapping/api/class-darkmatter-domains.php
+++ domain-mapping/api/class-darkmatter-domains.php
@@ -463,9 +463,9 @@ class DarkMatter_Domains {
 		 * When the domain is retrieved _and_ the domain is a "primary domain", then double-check that the primary
 		 * domain cache is set for it as well.
 		 */
-		$primary_cache_key = $_domain->blog_id . '-primary';
-		if ( $_domain->is_primary && empty( wp_cache_get( $primary_cache_key, 'dark-matter' ) ) ) {
-			wp_cache_set( $primary_cache_key, $_domain->domain, 'dark-matter' );
+		$primary_cache_key = $_domain['blog_id'] . '-primary';
+		if ( $_domain['is_primary'] && empty( wp_cache_get( $primary_cache_key, 'dark-matter' ) ) ) {
+			wp_cache_set( $primary_cache_key, $_domain['domain'], 'dark-matter' );
 		}
 
 		/**
