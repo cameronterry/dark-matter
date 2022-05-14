ce6b52a4b83335aad6376776911b1b5fb33edddc
Cameron Terry <cameronterry@protonmail.com>
2022-05-14 13:11:30 +0100

    Switched Policy interface for an abstract class.

---
+++ includes/classes/AdvancedCache/Policies/AbstractPolicy.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Scaffold for creating Policies for Dark Matter Advanced Cache.
+ *
+ * @package DarkMatter\AdvancedCache
+ */
+
+namespace DarkMatter\AdvancedCache\Policies;
+
+use DarkMatter\AdvancedCache\Data\Request;
+use DarkMatter\AdvancedCache\Processor\Visitor;
+
+/**
+ * Class AbstractPolicy
+ */
+abstract class AbstractPolicy {
+	/**
+	 * Response.
+	 *
+	 * @var string
+	 */
+	protected $response = '';
+
+	/**
+	 * Stop cache.
+	 *
+	 * @var bool
+	 */
+	protected $stop_cache = false;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param Request $request Details on the request.
+	 * @param Visitor $visitor Details on the visitor.
+	 */
+	public abstract function __construct( Request $request, Visitor $visitor );
+
+	/**
+	 * Prevent matching requests being cached.
+	 *
+	 * @return bool True to prevent / stop. False to continue.
+	 */
+	public function stop_cache() {
+		return $this->stop_cache;
+	}
+
+	/**
+	 * Provide a response for a policy. If a value is provided, then this will override the response and stop any
+	 * processing by any matching request.
+	 *
+	 * @return string
+	 */
+	public function response() {
+		return $this->response;
+	}
+}
