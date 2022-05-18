35dffb191d88da45cd1e8380af8986fd0413c454
Cameron Terry <cameronterry@protonmail.com>
2022-05-18 20:18:32 +0100

    Added the stub for Invalidation class.

---
+++ includes/classes/AdvancedCache/Admin/Invalidation.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Invalidation mechanism when content is updated.
+ *
+ * @package DarkMatter\AdvancedCache
+ */
+
+namespace DarkMatter\AdvancedCache\Admin;
+
+use DarkMatter\Interfaces\Registerable;
+
+/**
+ * Class Invalidation
+ */
+class Invalidation implements Registerable {
+	/**
+	 * Register hooks for actions and filters.
+	 *
+	 * @return void
+	 */
+	public function register() {
+		
+	}
+}
