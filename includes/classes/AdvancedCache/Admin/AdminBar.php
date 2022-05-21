45441deaa7ec05d7994ebb043efab733856747cc
Cameron Terry <cameronterry@protonmail.com>
2022-05-21 09:30:10 +0100

    Reworking the admin bar structure to actually match the way entries are stored.

--- includes/classes/AdvancedCache/Admin/AdminBar.php
+++ includes/classes/AdvancedCache/Admin/AdminBar.php
@@ -7,6 +7,7 @@
 namespace DarkMatter\AdvancedCache\Admin;
 
 use DarkMatter\AdvancedCache\Data\Request;
+use DarkMatter\AdvancedCache\Data\ResponseEntry;
 use DarkMatter\Interfaces\Registerable;
 
 /**
@@ -40,6 +41,7 @@ class AdminBar implements Registerable {
 	 */
 	public function menu( $admin_bar = null ) {
 		$request = new Request( $this->get_url() );
+		$variants_count = count( $request->variants );
 
 		/**
 		 * Add the parent node.
@@ -48,7 +50,11 @@ class AdminBar implements Registerable {
 			[
 				'id'    => 'dark-matter-advancedcache',
 				'title' => wp_kses(
-					__( '<span class="ab-icon dashicons dashicons-info"></span> Advanced Cache', 'dark-matter' ),
+					sprintf(
+						/* translators: %s: count of the number request variants. */
+						_n( 'Advanced Cache (%s variant stored)', 'Advanced Cache (%s variants stored)', $variants_count, 'dark-matter' ),
+						number_format_i18n( $variants_count )
+					),
 					[
 						'span' => [
 							'class' => true,
@@ -58,34 +64,7 @@ class AdminBar implements Registerable {
 			]
 		);
 
-		$this->menu_ttl( $admin_bar, $request );
 		$this->menu_variants( $admin_bar, $request );
-	}
-
-	/**
-	 * Handle the admin bar options for TTL.
-	 *
-	 * @param \WP_Admin_Bar $admin_bar Admin Bar.
-	 * @param Request       $request   Request.
-	 * @return void
-	 */
-	private function menu_ttl( $admin_bar, $request ) {
-		$when = __( 'None (Perpetual)', 'dark-matter' );
-		if ( 0 < $request->expiry ) {
-			$when = human_time_diff( wp_date( 'U' ), $request->expiry );
-		}
-
-		$admin_bar->add_node(
-			[
-				'id'     => 'dark-matter-advancedcache-ttl',
-				'parent' => 'dark-matter-advancedcache',
-				'title'  => sprintf(
-					/* translators: %s */
-					__( 'TTL: %s', 'dark-matter' ),
-					$when
-				),
-			]
-		);
 	}
 
 	/**
