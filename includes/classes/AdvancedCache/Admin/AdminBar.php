9f1eb58302916379a3bbb0d3121af63737ba95c4
Cameron Terry <cameronterry@protonmail.com>
2022-05-21 08:53:26 +0100

    Added a method to get the current URL.

--- includes/classes/AdvancedCache/Admin/AdminBar.php
+++ includes/classes/AdvancedCache/Admin/AdminBar.php
@@ -6,12 +6,32 @@
  */
 namespace DarkMatter\AdvancedCache\Admin;
 
+use DarkMatter\AdvancedCache\Data\Request;
 use DarkMatter\Interfaces\Registerable;
 
 /**
  * Class AdminBar
  */
 class AdminBar implements Registerable {
+	/**
+	 * Retrieves the URL for the current request.
+	 *
+	 * @return string URL, minus the protocol.
+	 */
+	private function get_url() {
+		/**
+		 * Retrieve the current request.
+		 */
+		global $wp;
+		$url = get_home_url( null, $wp->request );
+
+		/**
+		 * Remote the protocol.
+		 */
+		$protocol = wp_parse_url( $url, PHP_URL_SCHEME );
+		return str_replace( $protocol, '', $url );
+	}
+
 	/**
 	 * Handle hooks for the Admin Bar additions.
 	 *
