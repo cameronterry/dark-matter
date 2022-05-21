c92a27292aa5296d35f66df050477147219ef3cf
Cameron Terry <cameronterry@protonmail.com>
2022-05-21 11:41:00 +0100

    Created a mechanism to display the variant information on the admin bar.

--- includes/classes/AdvancedCache/Admin/AdminBar.php
+++ includes/classes/AdvancedCache/Admin/AdminBar.php
@@ -40,7 +40,9 @@ class AdminBar implements Registerable {
 	 * @return void
 	 */
 	public function menu( $admin_bar = null ) {
-		$request = new Request( $this->get_url() );
+		$url = $this->get_url();
+
+		$request = new Request( $url );
 		$variants_count = count( $request->variants );
 
 		/**
@@ -52,7 +54,7 @@ class AdminBar implements Registerable {
 				'title' => wp_kses(
 					sprintf(
 						/* translators: %s: count of the number request variants. */
-						_n( 'Advanced Cache (%s variant stored)', 'Advanced Cache (%s variants stored)', $variants_count, 'dark-matter' ),
+						_n( 'Advanced Cache (%s variant)', 'Advanced Cache (%s variants)', $variants_count, 'dark-matter' ),
 						number_format_i18n( $variants_count )
 					),
 					[
@@ -65,6 +67,74 @@ class AdminBar implements Registerable {
 		);
 
 		$this->menu_variants( $admin_bar, $request );
+
+		$admin_bar->add_node(
+			[
+				'id'     => 'dark-matter-advancedcache-clear',
+				'parent' => 'dark-matter-advancedcache',
+				'title'  => __( 'Clear Cache', 'dark-matter' ),
+			]
+		);
+	}
+
+	/**
+	 * Add to the menu the individual options for the variants.
+	 *
+	 * @param \WP_Admin_Bar $admin_bar Admin Bar.
+	 * @param Request       $request   Request.
+	 * @return void
+	 */
+	private function menu_variant( $admin_bar, $request, $variant = '' ) {
+		$response_entry = $request->get_variant( $variant );
+		if ( empty( $response_entry ) ) {
+			return;
+		}
+
+		$id = 'dark-matter-ac-variant-default';
+		if ( empty( $variant ) ) {
+			$id = 'dark-matter-ac-variant-' . esc_attr( $variant );
+		}
+
+		/**
+		 * Add the Variant parent node.
+		 */
+		$admin_bar->add_node(
+			[
+				'id'     => $id,
+				'parent' => 'dark-matter-advancedcache',
+				'title'  => __( 'Variant: Default', 'dark-matter' ),
+			]
+		);
+
+		/**
+		 * Add the Expiry node.
+		 */
+		$expiry = __( 'None (Perpetual)', 'dark-matter' );
+		if ( time() > $response_entry->expiry ) {
+			$expiry = __( 'Expired', 'dark-matter' );
+		} elseif ( 0 < $response_entry->expiry ) {
+			$expiry = human_time_diff( $response_entry->expiry, wp_date( 'u' ) );
+		}
+
+		$admin_bar->add_node(
+			[
+				'id'     => $id . '-expiry',
+				'parent' => $id,
+				'title'  => sprintf(
+					/* translators: %s: expiry time, in human readable (i.e. 5 minutes) */
+					__( 'Expiry: %s', 'dark-matter' ),
+					$expiry
+				),
+			]
+		);
+
+		$admin_bar->add_node(
+			[
+				'id'     => $id . '-clear',
+				'parent' => $id,
+				'title'  => __( 'Clear Variant', 'dark-matter' ),
+			]
+		);
 	}
 
 	/**
@@ -75,19 +145,48 @@ class AdminBar implements Registerable {
 	 * @return void
 	 */
 	private function menu_variants( $admin_bar, $request ) {
-		$variants_count = count( $request->variants );
+		$this->menu_variant( $admin_bar, $request );
 
-		$admin_bar->add_node(
-			[
-				'id'     => 'dark-matter-advancedcache-variants',
-				'parent' => 'dark-matter-advancedcache',
-				'title'  => sprintf(
-					/* translators: %s: count of the number request variants. */
-					_n( '%s variant stored', '%s variants stored', $variants_count, 'dark-matter' ),
-					number_format_i18n( $variants_count )
-				),
-			]
-		);
+		foreach ( $request->variants as $variant => $value ) {
+			/**
+			 * Get the response entry and ensure it hasn't been removed.
+			 */
+			$response_entry = $request->get_variant( $variant );
+			if ( empty( $response_entry ) ) {
+				continue;
+			}
+
+			$id = 'dark-matter-advancedcache-' . esc_attr( $variant );
+
+			$admin_bar->add_node(
+				[
+					'id'     => $id,
+					'parent' => 'dark-matter-advancedcache',
+					'title'  => sprintf(
+						/* translators: %s: variant name. */
+						__( 'Variant: %s', 'dark-matter' ),
+						$variant
+					),
+				]
+			);
+
+			$expiry = __( 'None (Perpetual)', 'dark-matter' );
+			if ( 0 < $response_entry->expiry ) {
+				$expiry = human_time_diff( $response_entry->expiry, wp_date( 'u' ) );
+			}
+
+			$admin_bar->add_node(
+				[
+					'id'     => $id . '-expiry',
+					'parent' => $id,
+					'title'  => sprintf(
+						/* translators: %s: expiry time, in human readable (i.e. 5 minutes) */
+						__( 'Expiry: %s', 'dark-matter' ),
+						$expiry
+					),
+				]
+			);
+		}
 	}
 
 	/**
