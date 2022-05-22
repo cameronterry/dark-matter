55b3898d19c49f9aa1860ce4437f5c69443ee5d1
Cameron Terry <cameronterry@protonmail.com>
2022-05-22 09:14:30 +0100

    Added a context parameter to storage methods, to distinguish "response" and "request" data.

--- includes/classes/AdvancedCache/Storage/AbstractStorage.php
+++ includes/classes/AdvancedCache/Storage/AbstractStorage.php
@@ -14,20 +14,22 @@ abstract class AbstractStorage {
 	/**
 	 * Delete a value in cache.
 	 *
-	 * @param string $key   The key under which to store the value.
-	 * @param string $group The group value appended to the $key.
+	 * @param string $key     The key under which to store the value.
+	 * @param string $group   The group value appended to the $key.
+	 * @param string $context Which cache data is being handled.
 	 * @return bool True on success. False otherwise.
 	 */
-	abstract public function delete( $key, $group );
+	abstract public function delete( $key, $group, $context = 'response' );
 
 	/**
 	 * Retrieve a value in cache.
 	 *
-	 * @param string $key   The key under which to store the value.
-	 * @param string $group The group value appended to the $key.
+	 * @param string $key     The key under which to store the value.
+	 * @param string $group   The group value appended to the $key.
+	 * @param string $context Which cache data is being handled.
 	 * @return mixed Cached value.
 	 */
-	abstract public function get( $key, $group );
+	abstract public function get( $key, $group, $context = 'response' );
 
 	/**
 	 * Add / update value in cache.
@@ -36,7 +38,8 @@ abstract class AbstractStorage {
 	 * @param mixed  $value      The value to store.
 	 * @param string $group      The group value appended to the $key.
 	 * @param int    $expiration The expiration time, defaults to 0 (infinite / until modified).
+	 * @param string $context    Which cache data is being handled.
 	 * @return bool True on success. False otherwise.
 	 */
-	abstract public function set( $key, $value, $group, $expiration = 0 );
+	abstract public function set( $key, $value, $group, $expiration = 0, $context = 'response' );
 }
--- includes/classes/AdvancedCache/Storage/WPCacheStorage.php
+++ includes/classes/AdvancedCache/Storage/WPCacheStorage.php
@@ -14,22 +14,24 @@ class WPCacheStorage extends AbstractStorage {
 	/**
 	 * Delete a value in cache.
 	 *
-	 * @param string $key   The key under which to store the value.
-	 * @param string $group The group value appended to the $key.
+	 * @param string $key     The key under which to store the value.
+	 * @param string $group   The group value appended to the $key.
+	 * @param string $context Which cache data is being handled.
 	 * @return bool True on success. False otherwise.
 	 */
-	public function delete( $key, $group ) {
+	public function delete( $key, $group, $context = 'response' ) {
 		return wp_cache_delete( $key, $group );
 	}
 
 	/**
 	 * Retrieve a value in cache.
 	 *
-	 * @param string $key   The key under which to store the value.
-	 * @param string $group The group value appended to the $key.
+	 * @param string $key     The key under which to store the value.
+	 * @param string $group   The group value appended to the $key.
+	 * @param string $context Which cache data is being handled.
 	 * @return mixed Cached value.
 	 */
-	public function get( $key, $group ) {
+	public function get( $key, $group, $context = 'response' ) {
 		return wp_cache_get( $key, $group );
 	}
 
@@ -40,9 +42,10 @@ class WPCacheStorage extends AbstractStorage {
 	 * @param mixed  $value      The value to store.
 	 * @param string $group      The group value appended to the $key.
 	 * @param int    $expiration The expiration time, defaults to 0 (infinite / until modified).
+	 * @param string $context    Which cache data is being handled.
 	 * @return bool True on success. False otherwise.
 	 */
-	public function set( $key, $value, $group, $expiration = 0 ) {
+	public function set( $key, $value, $group, $expiration = 0, $context = 'response' ) {
 		return wp_cache_set( $key, $value, $group, $expiration );
 	}
 }
