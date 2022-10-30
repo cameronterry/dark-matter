9870fffd1962afa7f8cf401509ef54f987cb96a3
Cameron Terry <cameron.terry@10up.com>
2022-10-30 16:37:05 +0000

    Added the logic to set the global variables.

--- includes/classes/DomainMapping/Sunrise.php
+++ includes/classes/DomainMapping/Sunrise.php
@@ -1,9 +1,18 @@
 <?php
 /**
  * Handles the logic for WordPress dropin plugin, `sunrise.php`.
+ *
+ * @since 3.0.0
+ *
+ * @package DarkMatterPlugin
+ *
+ * @phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
  */
 
 namespace DarkMatter\DomainMapping;
+
+use DarkMatter\DomainMapping\Data;
+use DarkMatter\DomainMapping\Manager;
 
 /**
  * Class Sunrise
@@ -18,11 +27,33 @@ class Sunrise {
 	 */
 	public function __construct() {
 		$this->init();
+
+		/**
+		 * Find the domain based on the request.
+		 */
+		$domain = $this->get_domain();
+		if ( $domain && $domain->active && $this->set_globals( $domain ) ) {
+
+		}
+	}
+
+	/**
+	 * Get the domain from the requested FQDN.
+	 *
+	 * @since 3.0.0
+	 *
+	 * @return bool|Data\Domain
+	 */
+	private function get_domain() {
+		$fqdn = Helper::instance()->get_request_fqdn();
+		return Manager\Domain::instance()->get( $fqdn );
 	}
 
 	/**
 	 * Set up various globals and constants which are used, either by Dark Matter itself or by other plugins and/or
 	 * WordPress Core.
+	 *
+	 * @since 3.0.0
 	 *
 	 * @return void
 	 */
@@ -44,5 +75,48 @@ class Sunrise {
 		 * Use to detect misconfiguration where the cookie domain is set.
 		 */
 		define( 'DARKMATTER_COOKIE_SET', ! defined( 'COOKIE_DOMAIN' ) );
+	}
+
+	/**
+	 * Sets up a few global variables which are used through WordPress Core. Will do some basic detection to ensure the
+	 * website should be served on the mapped domain.
+	 *
+	 * @since 3.0.0
+	 *
+	 * @param Data\Domain $domain Domain data.
+	 * @return bool True to proceed, false to disengage.
+	 */
+	private function set_globals( $domain ) {
+		global $blog_id, $current_blog, $current_site, $site_id;
+
+		/**
+		 * Set the current Blog (WP_Site) and current Site (WP_Network).
+		 */
+		$current_blog = get_site( $domain->blog_id );
+		$blog_id      = $current_blog->blog_id;
+		$current_site = \WP_Network::get_instance( $current_blog->site_id );
+		$site_id      = $current_blog->site_id;
+
+		/**
+		 * Make sure the current blog is public.
+		 */
+		if (
+			/**
+			 * Check the current blog is public and be compatible with plugins such as Restricted Site Access.
+			 */
+			0 > (int) $current_blog->public
+			/**
+			 * Check the current blog has not been archived.
+			 */
+			&& 0 !== (int) $current_blog->archived
+			/**
+			 * Check the current blog has not been "soft" deleted.
+			 */
+			&& 0 !== (int) $current_blog->deleted
+		) {
+			return false;
+		}
+
+		return true;
 	}
 }
