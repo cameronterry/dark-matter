e2c99997d404e369cbea7d937fd5a819e8f52b12
Cameron Terry <cameron.terry@10up.com>
2022-10-30 16:55:35 +0000

    Created new class to house the Redirect.

---
+++ includes/classes/DomainMapping/Processor/Redirect.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Processor for determining if a redirect is required.
+ *
+ * @since 3.0.0
+ *
+ * @package DarkMatterPlugin\Domain
+ */
+
+namespace DarkMatter\DomainMapping\Processor;
+
+use DarkMatter\Interfaces\Registerable;
+
+/**
+ * Class Redirect
+ *
+ * @since 3.0.0
+ */
+class Redirect implements Registerable {
+	/**
+	 * Register hooks for this class.
+	 *
+	 * @since 3.0.0
+	 *
+	 * @return void
+	 */
+	public function register() {
+	}
+}
