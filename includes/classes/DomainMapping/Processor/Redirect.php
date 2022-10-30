218aeb28f1b50f21f59152cb4e514806af0bea0a
Cameron Terry <cameron.terry@10up.com>
2022-10-30 17:00:44 +0000

    Added the logic for the can_redirect() method.

--- includes/classes/DomainMapping/Processor/Redirect.php
+++ includes/classes/DomainMapping/Processor/Redirect.php
@@ -25,7 +25,53 @@ class Redirect implements Registerable {
 	 * @return bool True to redirect, false otherwise.
 	 */
 	private function can_redirect() {
-		return false;
+		/**
+		 * The function `rest_get_url_prefix()` is not available at this point in the load process. Therefore, we must
+		 * substitute it with a close approximate of what the function does.
+		 *
+		 * There is a side effect of this. Basically if a site is to be setup with a different prefix, in order for this
+		 * to work, the `add_filter()` call would need to be done in a Must-Use Plugin.
+		 *
+		 * @see https://developer.wordpress.org/reference/hooks/rest_url_prefix/
+		 */
+		$rest_url_prefix = '/' . trim( apply_filters( 'rest_url_prefix', 'wp-json' ), '/' ) . '/';
+
+		return ! (
+			/**
+			 * Do not attempt to redirect during the CLI command.
+			 */
+			( defined( 'WP_CLI' ) && WP_CLI )
+			||
+			/**
+			 * AJAX requests can be used on both the mapped and unmapped domains.
+			 */
+			( defined( 'DOING_AJAX' ) && DOING_AJAX )
+			||
+			/**
+			 * Do not attempt to redirect during the execution of cron.
+			 */
+			( defined( 'DOING_CRON' ) && DOING_CRON )
+			||
+			/**
+			 * XMLRPC Requests can be used on both the mapped and unmapped domains.
+			 */
+			( defined( 'XMLRPC_REQUEST' ) && XMLRPC_REQUEST )
+			||
+			/**
+			 * REST API can be used on both the mapped and unmapped domains.
+			 */
+			( ! empty( $_SERVER['REQUEST_URI'] ) && false !== strpos( $_SERVER['REQUEST_URI'], $rest_url_prefix ) ) // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
+			||
+			/**
+			 * Customizer is presented in an <iframe> over the unmapped domain.
+			 */
+			! empty( $_GET['customize_changeset_uuid'] ) // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+			||
+			/**
+			 * Do not redirect Previews
+			 */
+			( ! empty( $_GET['preview'] ) || ! empty( $_GET['page_id'] ) || ! empty( $_GET['p'] ) ) // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+		);
 	}
 
 	/**
