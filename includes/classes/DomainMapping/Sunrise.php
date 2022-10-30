9ca06f67d6bbd759c173f09a7217790aa235c5b1
Cameron Terry <cameron.terry@10up.com>
2022-10-30 16:15:39 +0000

    Created a new class for handling Sunrise logic.

---
+++ includes/classes/DomainMapping/Sunrise.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Handles the logic for WordPress dropin plugin, `sunrise.php`.
+ */
+
+namespace DarkMatter\DomainMapping;
+
+/**
+ * Class Sunrise
+ *
+ * @since 3.0.0
+ */
+class Sunrise {
+	/**
+	 * Constructor.
+	 *
+	 * @since 3.0.0
+	 */
+	public function __construct() {
+
+	}
+}
