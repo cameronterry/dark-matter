800245d04bd0aef92023df1e681d9dd8a74bc6f8
Cameron Terry <cameron.terry@10up.com>
2022-10-30 16:22:31 +0000

    Added the logic for define global cache group and a few constants.

--- includes/classes/DomainMapping/Sunrise.php
+++ includes/classes/DomainMapping/Sunrise.php
@@ -17,6 +17,32 @@ class Sunrise {
 	 * @since 3.0.0
 	 */
 	public function __construct() {
+		$this->init();
+	}
 
+	/**
+	 * Set up various globals and constants which are used, either by Dark Matter itself or by other plugins and/or
+	 * WordPress Core.
+	 *
+	 * @return void
+	 */
+	private function init() {
+		/**
+		 * Ensure `dark-matter` cache group is available globally.
+		 */
+		wp_cache_add_global_groups( 'dark-matter' );
+
+		/**
+		 * Define the `SUNRISE_LOADED`. Dark Matter Plugin does not make use of this but other plugins do in certain
+		 * circumstances (i.e. Jetpack). So we define it for compatibility.
+		 */
+		if ( false === defined( 'SUNRISE_LOADED' ) ) {
+			define( 'SUNRISE_LOADED', true );
+		}
+
+		/**
+		 * Use to detect misconfiguration where the cookie domain is set.
+		 */
+		define( 'DARKMATTER_COOKIE_SET', ! defined( 'COOKIE_DOMAIN' ) );
 	}
 }
