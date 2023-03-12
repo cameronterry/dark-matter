6f386107ac5175ec0474902c8a0673e84b3de65e
Cameron <cameronterry@protonmail.com>
2023-03-12 22:48:45 +0000

    Added logic for the Authorise to get the URL data.

--- includes/classes/SSO/Authorise.php
+++ includes/classes/SSO/Authorise.php
@@ -14,11 +14,41 @@ use DarkMatter\Interfaces\Registerable;
  */
 class Authorise implements Registerable {
 	/**
+	 * URL data.
+	 *
+	 * @var array
+	 */
+	private $data = [];
+
+	/**
 	 * Handle actions and filters for this SSO Authorise.
 	 *
 	 * @return void
 	 */
 	public function register() {
+		$this->data = filter_var_array(
+			$_GET,
+			[
+				'action' => [
+					'filter'  => FILTER_CALLBACK,
+					'options' => 'sanitize_text_field',
+				],
+				'nonce'  => [
+					'filter'  => FILTER_CALLBACK,
+					'options' => 'sanitize_text_field',
+				],
+				'token'  => [
+					'filter'  => FILTER_CALLBACK,
+					'options' => 'sanitize_text_field',
+				],
+			]
+		);
 
+		/**
+		 * Check if the current request has data and has the action.
+		 */
+		if ( empty( $this->data ) || 'dmp_auth_check' === $this->data['action'] ) {
+			return;
+		}
 	}
 }
