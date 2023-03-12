67dbf0d8c4e1d613c75865a9871bf8de900e7579
Cameron <cameronterry@protonmail.com>
2023-03-12 22:43:49 +0000

    Created a stub for the Authorisation process.

---
+++ includes/classes/SSO/Authorise.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Handle the SSO authorisation flow.
+ *
+ * @package DarkMatterPlugin\SSO
+ */
+
+namespace DarkMatter\SSO;
+
+use DarkMatter\Interfaces\Registerable;
+
+/**
+ * Class Authorise
+ */
+class Authorise implements Registerable {
+	/**
+	 * Handle actions and filters for this SSO Authorise.
+	 *
+	 * @return void
+	 */
+	public function register() {
+
+	}
+}
