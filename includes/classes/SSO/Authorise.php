a0a60ed9c8a0d3f9df315ee569486d08b8b8541f
Cameron <cameronterry@protonmail.com>
2023-03-12 23:04:00 +0000

    Added a handle method for the visitor-side request.

--- includes/classes/SSO/Authorise.php
+++ includes/classes/SSO/Authorise.php
@@ -46,11 +46,51 @@ class Authorise implements Registerable {
 	}
 
 	/**
+	 * Handle the authorise request.
+	 *
+	 * @return void
+	 */
+	public function handle() {
+		/**
+		 * Check to see if the token exists.
+		 */
+		$token_data = Token::instance()->get( $this->data['token'], 'token' );
+		if ( empty( $token_data['nonce'] ) && empty( $token_data['user_id'] ) ) {
+			return;
+		}
+
+		/**
+		 * Check the nonce matches.
+		 */
+		if ( $this->data['nonce'] !== $token_data['nonce'] ) {
+			return;
+		}
+
+		$url = admin_url();
+		$url = add_query_arg(
+			[
+				'action' => 'dmp_auth_check',
+				'token'  => $this->data,
+			],
+			$url
+		);
+
+		wp_safe_redirect( $url, 302, 'Dark-Matter-Plugin' );
+	}
+
+	/**
 	 * Handle actions and filters for this SSO Authorise.
 	 *
 	 * @return void
 	 */
 	public function register() {
+		/**
+		 * If they are already logged in, then our work here is done.
+		 */
+		if ( is_user_logged_in() ) {
+			return;
+		}
+
 		$this->get_data();
 
 		/**
@@ -59,5 +99,7 @@ class Authorise implements Registerable {
 		if ( empty( $this->data ) || 'dmp_auth_check' === $this->data['action'] ) {
 			return;
 		}
+
+		add_action( 'template_redirect', [ $this, 'handle' ] );
 	}
 }
