0fc440fbccb48faefab1011240fa97aaf958f213
Cameron <cameronterry@protonmail.com>
2023-03-18 19:06:55 +0000

    Added a stub class for handling the JS side of SSO.

---
+++ includes/classes/SSO/Script.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Handle the logic for making the SSO automatic.
+ *
+ * @package DarkMatterPlugin
+ */
+
+namespace DarkMatter\SSO;
+
+use DarkMatter\Interfaces\Registerable;
+
+/**
+ * Class Script
+ */
+class Script implements Registerable {
+	/**
+	 * Handle actions and filters for SSO JavaScript.
+	 *
+	 * @return void
+	 */
+	public function register() {
+		
+	}
+}
