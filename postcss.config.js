a092daf595c724a47a5c41dfb1297dfd0079cadf
Cameron <cameronterry@protonmail.com>
2023-05-07 16:29:25 +0100

    Simplified node packages / webpack / build process.

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,7 @@
 module.exports = ( { env } ) => ( {
   plugins: {
-    'postcss-import': {},
     'postcss-preset-env': {
-      autoprefixer: {
-        grid: true
-      },
       browsers: 'last 2 versions',
-      preserve: false,
-      stage: 0
-    },
-    'cssnano':
-      'production' === env
-        ? {}
-        : false
-    ,
+    }
   },
 } );
