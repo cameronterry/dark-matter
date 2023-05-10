48e2096da40aca437148a32b3de3d0f46a04c455
Cameron <cameronterry@protonmail.com>
2023-05-10 22:15:09 +0100

    Updated the Card control to make use of the new elements and classes.

--- assets/js/components/domain/Card.js
+++ assets/js/components/domain/Card.js
@@ -1,9 +1,13 @@
+/* eslint-disable camelcase */
+
 /**
  * WordPress Dependencies.
  *
  * @param props
  */
-// import { __ } from '@wordpress/i18n';
+import { __ } from '@wordpress/i18n';
+
+import { ToggleControl } from '../controls/ToggleControl';
 
 /**
  * Card component.
@@ -12,12 +16,35 @@
  * @return {JSX.Element} Card JSX.
  */
 export const Card = ( props ) => {
-	const { domain } = props;
+	const { domain, is_active, is_https, is_primary, type } = props;
+
+	const types = {
+		1: __( 'Main', 'darkmatterplugin' ),
+		2: __( 'Media', 'darkmatterplugin' ),
+	};
+
+	const typeDisplay = is_primary
+		? __( 'Primary', 'darkmatterplugin' )
+		: types[ type ]
+	;
 
 	return (
 		<div className="dmp__domain-card">
 			<h2>{ domain }</h2>
-
+			<div className="dmp__control-info">
+				<span>{ __( 'Type: ', 'darkmatterplugin' ) }</span>
+				<span>{ typeDisplay }</span>
+			</div>
+			<div className="dmp__control-info">
+				<span>{ __( 'Protocol: ', 'darkmatterplugin' ) }</span>
+				<span className={ is_https ? 'success' : 'warning' }>{ is_https ? __( 'https://', 'darkmatterplugin' ) : __( 'http://', 'darkmatterplugin' ) }</span>
+			</div>
+			<ToggleControl checked={ is_primary } label={ __( 'Primary', 'darkmatterplugin' ) } onChange={ ( e ) => {
+				console.log( e ); // eslint-disable-line
+			} } />
+			<ToggleControl checked={ is_active } label={ __( 'Active', 'darkmatterplugin' ) } onChange={ ( e ) => {
+				console.log( e ); // eslint-disable-line
+			} } />
 		</div>
 	);
 };
