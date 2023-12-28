41249300236e515632512a3807a6e77b426c45a1
Cameron <cameronterry@protonmail.com>
2023-12-28 14:49:00 +0000

    Completed the form and added the buttons.

--- assets/js/components/domain/modals/NewDomain.js
+++ assets/js/components/domain/modals/NewDomain.js
@@ -7,6 +7,7 @@
  * WordPress dependencies
  */
 import {
+	Button,
 	Modal,
 	TextControl,
 	ToggleControl,
@@ -53,31 +54,64 @@ export const NewDomainModal = ( {
 				size="medium"
 				title={ __( 'Add New Domain', 'darkmatterplugin' ) }
 			>
-				<TextControl
-					label={ __( 'Domain', 'darkmatterplugin' ) }
-					onChange={ () => {
-						setNewDomain( {} );
-					} }
-					value={ domain }
-				/>
-				<ToggleControl
-					label={ __( 'Is Primary?', 'darkmatterplugin' ) }
-					onChange={ () => {
-						setNewDomain( { is_primary: ! is_primary } );
-					} }
-				/>
-				<ToggleControl
-					label={ __( 'Is HTTPS?', 'darkmatterplugin' ) }
-					onChange={ () => {
-						setNewDomain( { is_https: ! is_https } );
-					} }
-				/>
-				<ToggleControl
-					label={ __( 'Is Active?', 'darkmatterplugin' ) }
-					onChange={ () => {
-						setNewDomain( { is_active: ! is_active } );
-					} }
-				/>
+				<form>
+					<TextControl
+						label={ __( 'Domain', 'darkmatterplugin' ) }
+						onChange={ ( value ) => {
+							setNewDomain( { ...newDomain, domain: value } );
+						} }
+						value={ domain }
+					/>
+					<ToggleControl
+						checked={ is_primary }
+						help={
+							is_primary
+								? __( 'Your website will use this domain. All other main domains will redirect to this domain.', 'darkmatterplugin' )
+								: __( 'A secondary domain. Requests to this domain will be redirected to the primary domain.', 'darkmatterplugin' )
+						}
+						label={ __( 'Is Primary?', 'darkmatterplugin' ) }
+						onChange={ () => {
+							setNewDomain( { ...newDomain, is_primary: ! is_primary } );
+						} }
+					/>
+					<ToggleControl
+						checked={ is_https }
+						help={
+							is_https
+								? __( 'Use HTTPS', 'darkmatterplugin' )
+								: __( 'Use HTTP. Note: not recommended as it will decrease the security and SEO of your website.', 'darkmatterplugin' )
+						}
+						label={ __( 'Is HTTPS?', 'darkmatterplugin' ) }
+						onChange={ () => {
+							setNewDomain( { ...newDomain, is_https: ! is_https } );
+						} }
+					/>
+					<ToggleControl
+						checked={ is_active }
+						help={
+							is_active
+								? ''
+								: __( 'Redirects will not engaged. Useful for testing purposes.', 'darkmatterplugin' )
+						}
+						label={ __( 'Is Active?', 'darkmatterplugin' ) }
+						onChange={ () => {
+							setNewDomain( { ...newDomain, is_active: ! is_active } );
+						} }
+					/>
+					<div className="dmp__domain-modal-buttons">
+						<Button
+							onClick={ closeModal }
+							variant="teritary"
+						>
+							{ __( 'Cancel', 'darkmatterplugin' ) }
+						</Button>
+						<Button
+							variant="primary"
+						>
+							{ __( 'Add Domain', 'darkmatterplugin' ) }
+						</Button>
+					</div>
+				</form>
 			</Modal>
 		</>
 	);
