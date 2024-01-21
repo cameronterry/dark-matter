20fe4c39e403155420af50a4c6a11b0caa1368ff
Cameron <cameronterry@protonmail.com>
2024-01-21 19:11:17 +0000

    Moved Restricted Domain custom table to the new class.

---
+++ includes/classes/DomainMapping/Data/RestrictedDomain.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Handling the custom table for storing Restricted Domains information.
+ *
+ * @package DarkMatterPlugin\DomainMapping\Data
+ */
+
+namespace DarkMatter\DomainMapping\Data;
+
+use DarkMatter\Helper\CustomTable;
+
+/**
+ * Class RestrictedDomain
+ */
+class RestrictedDomain extends CustomTable {
+	/**
+	 * Columns definition for the Restricted Domain table.
+	 *
+	 * @return array
+	 */
+	protected function get_columns() {
+		return [
+			'id'         => [
+				'autoincrement' => true,
+				'nullable'      => false,
+				'queryable'     => false,
+				'type'          => 'BIGINT',
+				'type_storage'  => 20,
+			],
+			'domain'     => [
+				'nullable'      => false,
+				'queryable'     => false,
+				'type'          => 'VARCHAR',
+				'type_storage'  => 255,
+			],
+		];
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	protected function get_indexes() {
+		return false;
+	}
+
+	/**
+	 * Restricted Domains primary key.
+	 *
+	 * @return string
+	 */
+	protected function get_primary_key() {
+		return 'id';
+	}
+
+	/**
+	 * Restricted Domains table name.
+	 *
+	 * @return string
+	 */
+	protected function get_tablename() {
+		global $wpdb;
+		return $wpdb->base_prefix . 'domain_restrict';
+	}
+}
