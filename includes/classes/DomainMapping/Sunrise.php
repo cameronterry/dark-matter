65a4fc43a379d58a990e563a4199a79712a0802b
Cameron <cameronterry@protonmail.com>
2024-05-12 14:53:28 +0100

    Updated Sunrise to use the new DomainQuery.

--- includes/classes/DomainMapping/Sunrise.php
+++ includes/classes/DomainMapping/Sunrise.php
@@ -11,8 +11,6 @@
 
 namespace DarkMatter\DomainMapping;
 
-use DarkMatter\DomainMapping\Data;
-use DarkMatter\DomainMapping\Manager;
 use DarkMatter\DomainMapping\Processor\Redirect;
 
 /**
@@ -52,8 +50,9 @@ class Sunrise {
 	 * @return bool|Data\Domain
 	 */
 	private function get_domain() {
-		$fqdn = Helper::instance()->get_request_fqdn();
-		return Manager\Domain::instance()->get( $fqdn );
+		$fqdn  = Helper::instance()->get_request_fqdn();
+		$query = new Data\DomainQuery();
+		return $query->get_by_domain( $fqdn );
 	}
 
 	/**
