6f87fb3394de58f7f8983c90a148686963697366
Cameron <cameronterry@protonmail.com>
2025-02-16 11:32:14 +0000

    Implemented the module.

--- includes/classes/Helper/Module.php
+++ includes/classes/Helper/Module.php
@@ -8,22 +8,95 @@
 
 namespace DarkMatter\Helper;
 
+use DarkMatter\Interfaces\Registerable;
+use HaydenPierce\ClassFinder\ClassFinder;
+
 /**
  * Class Module
  */
-class Module {
+abstract class Module {
 
 	/**
 	 * Instantiated classes.
 	 *
 	 * @var array
 	 */
-	private $classes = [];
+	protected $classes = [];
 
 	/**
-	 * Variable storage for the Singleton.
+	 * Namespace for the classes of the Module to be loaded.
 	 *
-	 * @var null|Module
+	 * @var string
 	 */
-	private static $instance = null;
+	private $namespace = '';
+
+	/**
+	 * Constructor.
+	 *
+	 * @param string $namespace Namespace of the module, excluding the root namespace.
+	 */
+	protected function __construct( $namespace ) {
+		$this->namespace = sprintf( '\DarkMatter\%s', $namespace );
+	}
+
+	/**
+	 * Converts a class name into a slug: an array key friendly version, essentially.
+	 *
+	 * @param string $class_name Class name to convert into a slug.
+	 * @return string
+	 */
+	private function class_name_slug( $class_name ) {
+		return sanitize_title( str_replace( '\\', '--', $class_name ) );
+	}
+
+	/**
+	 * Retrieves all the classes within a specific namespace.
+	 *
+	 * @return string[] Classes within the namespace.
+	 * @throws \Exception
+	 */
+	private function get_classes() {
+		$finder = new ClassFinder();
+		$finder::setAppRoot( DM_PATH . '/' );
+
+		return $finder::getClassesInNamespace( $this->namespace, ClassFinder::RECURSIVE_MODE );
+	}
+
+	/**
+	 * Loads all the classes within the Module namespace.
+	 *
+	 * @return void
+	 * @throws \ReflectionException
+	 */
+	public function load() {
+		foreach ( $this->get_classes() as $class ) {
+			/**
+			 * Check and ensure we do not double-load a class.
+			 */
+			$slug = $this->class_name_slug( $class );
+			if ( isset( $this->classes[ $slug ] ) ) {
+				continue;
+			}
+
+			$reflection = new \ReflectionClass( $class );
+
+			/**
+			 * Make sure the class is one that can be worked with.
+			 */
+			if ( ! $reflection->isInstantiable() || ! $reflection->implementsInterface( '\DarkMatter\Interfaces\Registerable' ) ) {
+				continue;
+			}
+
+			/**
+			 * Instantiate the class and determine if we can register it.
+			 *
+			 * @var Registerable $class
+			 */
+			$class = new $class();
+			if ( $class->can_register() ) {
+				$this->classes[ $slug ] = $class;
+				$class->register();
+			}
+		}
+	}
 }
