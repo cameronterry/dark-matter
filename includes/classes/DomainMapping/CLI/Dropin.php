78f9909c98f8146ec5a7238214a4cc5567093166
Cameron <cameronterry@protonmail.com>
2025-09-26 18:02:32 +0100

    Updated the dropin command to use the new autoloader.

--- includes/classes/DomainMapping/CLI/Dropin.php
+++ includes/classes/DomainMapping/CLI/Dropin.php
@@ -1,23 +1,32 @@
 <?php
 /**
- * Class DarkMatter_Dropin_CLI
+ * Command for checking and updating Sunrise.
  *
  * @package DarkMatter
  * @since 2.0.0
  */
 
-defined( 'ABSPATH' ) || die;
+namespace DarkMatterPlugin\DomainMapping\CLI;
 
-if ( ! defined( 'WP_CLI' ) ) {
-	return;
-}
+use DarkMatterPlugin\Command;
+use WP_CLI;
 
 /**
- * Class DarkMatter_Dropin_CLI
+ * Class Dropin
  *
  * @since 2.0.0
  */
-class DarkMatter_Dropin_CLI {
+class Dropin implements Command {
+
+	/**
+	 * Register the dropin command class.
+	 *
+	 * @return bool
+	 */
+	public static function can_register() {
+		return true;
+	}
+
 	/**
 	 * Helper command to see if the Sunrise dropin plugin within Dark Matter is
 	 * the same version as in use on the current WordPress installation.
@@ -30,7 +39,7 @@ class DarkMatter_Dropin_CLI {
 	 * @since 2.0.0
 	 */
 	public function check() {
-		$health_check = DM_HealthChecks::instance();
+		$health_check = \DM_HealthChecks::instance();
 
 		if ( $health_check->is_dropin_latest() ) {
 			WP_CLI::success( __( 'Current Sunrise dropin matches the Sunrise within Dark Matter plugin.', 'dark-matter' ) );
@@ -99,5 +108,13 @@ class DarkMatter_Dropin_CLI {
 			WP_CLI::error( __( 'Unknown error occurred preventing the update of Sunrise dropin.', 'dark-matter' ) );
 		}
 	}
+
+	/**
+	 * Handle the registration of Dropin commands.
+	 *
+	 * @return void
+	 */
+	public static function register() {
+		WP_CLI::add_command( 'darkmatter dropin', self::class );
+	}
 }
-WP_CLI::add_command( 'darkmatter dropin', 'DarkMatter_Dropin_CLI' );
