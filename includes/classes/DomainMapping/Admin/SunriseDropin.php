daf325ad3fae23139c8fc8890b3e423c580d89bc
Cameron <cameronterry@protonmail.com>
2026-03-24 21:55:12 +0000

    Added logic to display a notice with actionable link for admins to update Sunrise.

---
+++ includes/classes/DomainMapping/Admin/SunriseDropin.php
@@ -0,0 +1,145 @@
+<?php
+/**
+ * Checks and provides an actionable notice for administrators relating to updates for the `sunrise.php` dropin plugin.
+ *
+ * @package DarkMatterPlugin
+ */
+
+namespace DarkMatterPlugin\DomainMapping\Admin;
+
+use DarkMatterPlugin\Registerable;
+
+/**
+ * Class SunriseDropin
+ */
+class SunriseDropin implements Registerable {
+
+	/**
+	 * Minimum permission capability to perform updates/handling for Sunrise dropin plugin.
+	 *
+	 * @var string
+	 */
+	private $permission_cap = 'manage_options';
+
+	/**
+	 * Register this class.
+	 *
+	 * @return bool
+	 */
+	public function can_register() {
+		return current_user_can( $this->permission_cap );
+	}
+
+	/**
+	 * Determines which version of the `sunrise.php` dropin is used.
+	 *
+	 * @return string
+	 */
+	public function get_dropin_version() {
+		$installed = WP_CONTENT_DIR . '/sunrise.php';
+		if ( ! file_exists( $installed ) ) {
+			return 'notfound';
+		}
+
+		$current   = DM_PATH . 'includes/dropins/sunrise.php';
+		$legacy    = DM_PATH . 'includes/legacy/sunrise.php';
+
+		$installed_size = filesize( $installed );
+		$installed_md5  = md5_file( $installed );
+
+		if ( $installed_size === filesize( $current ) && $installed_md5 === md5_file( $current ) ) {
+			return 'latest';
+		} elseif ( $installed_size === filesize( $legacy ) && $installed_md5 === md5_file( $legacy ) ) {
+			return 'legacy';
+		}
+
+		return 'unknown';
+	}
+
+	/**
+	 * Show a notice to applicable users who can perform an update for the Sunrise plugin.
+	 *
+	 * @return void
+	 */
+	public function maybe_show_notice() {
+		/**
+		 * Used by third party developer's to disable the check for the Sunrise dropin plugin. This can be because the
+		 * file is included in another manner or has been customised to include additional logic beyond Dark Matter
+		 * Plugin.
+		 *
+		 * @param bool $disable True to disable sunrise check. False otherwise.
+		 * @return bool
+		 */
+		if ( apply_filters( 'darkmatterplugin_sunrise_disable_check', false ) ) {
+			return;
+		}
+
+		if ( ! current_user_can( $this->permission_cap ) ) {
+			return;
+		}
+
+		$message = '';
+
+		/**
+		 * Adjust the message depending on the situation.
+		 */
+		$version = $this->get_dropin_version();
+		if ( 'latest' === $version ) {
+			return;
+		} elseif ( 'notfound' === $version ) {
+			$message = __( 'Sunrise dropin plugin could not be found.', 'darkmatterplugin' );
+		} elseif( 'legacy' === $version ) {
+			$message = __( 'An update is available for the Sunrise dropin plugin.', 'darkmatterplugin' );
+		}
+
+		if ( empty( $message ) ) {
+			return;
+		}
+
+		/**
+		 * If possible, provide a way for the user to update sunrise dropin.
+		 */
+		if ( is_writable( WP_CONTENT_DIR . '/sunrise.php' ) ) {
+			$action = sprintf(
+				/* translators: %s: URL used to perform the action. */
+				__( 'You can update the Sunrise dropin plugin by <a href="%s">clicking here</a>.', 'darkmatterplugin' ),
+				wp_nonce_url(
+					admin_url( '/admin.php?action=darkmatterplugin_update_dropin' ),
+					'darkmatterplugin_update_dropin'
+				)
+			);
+		} else {
+			$action = __( 'It is not possible to update Sunrise dropin. Please notify your system administrator or developer.', 'darkmatterplugin' );
+		}
+
+		?>
+		<div class="notice notice-error">
+			<?php
+			echo wp_kses(
+				sprintf(
+					'<p>%s</p><p>%s</p>',
+					$message,
+					$action
+				),
+				[
+					'a' => [
+						'href'  => [],
+						'title' => [],
+					],
+					'p' => [],
+				]
+			);
+			?>
+		</div>
+		<?php
+	}
+
+	/**
+	 * Handle actions and filters for the Sunrise Dropin checks and handling.
+	 *
+	 * @return void
+	 */
+	public function register() {
+		add_action( 'admin_notices', [ $this, 'maybe_show_notice' ] );
+	}
+}
